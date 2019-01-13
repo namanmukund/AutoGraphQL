a3060afbc1965e583fd0033ff213fbe9937ae207
admin_kp <admin_kp@delhis-MacBook-Pro.local>
2019-01-13 23:23:41 +0530

    fixing typo in Episode subtitle name

--- config/graphqlSchema/core/types/episode/Episode.js
+++ config/graphqlSchema/core/types/episode/Episode.js
@@ -2,7 +2,7 @@ const Episode = `
   type Episode @model {
     title: String! @unique
     description: String @unique
-    subtitle: File @relation(name: "EpisodeVideo", direction: "OneWay")
+    subtitle: File @relation(name: "EpisodeSubtitle", direction: "OneWay")
     video: File @relation(name: "EpisodeVideo", direction: "OneWay")
     topic: Topic @relation(name: "TopicEpisode")
   }
