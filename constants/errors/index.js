ad0f665886b14790cdd6f09792d3bdb97f5cc4ac
namanmukund <namanmukund@gmail.com>
2019-01-28 12:03:26 +0530

    lint error

--- constants/errors/index.js
+++ constants/errors/index.js
@@ -15,7 +15,7 @@ import {
   UserAlreadyExistsError,
   AdditionalFieldUpdateDeniedError,
   FileWriteError,
-  DeleteChapterError
+  DeleteChapterError,
 } from './db';
 import {
   RelationValuesExistError, ConnectMutationsArgumentsLimitError,
@@ -116,5 +116,5 @@ export {
   DatabaseRecordNotFoundError,
   ConnectRecordsNotFoundInDBError,
   UserAlreadyExistsError,
-  DeleteChapterError
+  DeleteChapterError,
 };
