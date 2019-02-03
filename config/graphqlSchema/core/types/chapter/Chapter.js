148938f0ed6116ea72593f04082f8f48bdc571d0
namanmukund <namanmukund@gmail.com>
2019-02-03 19:36:47 +0530

    min max length check on field

--- config/graphqlSchema/core/types/chapter/Chapter.js
+++ config/graphqlSchema/core/types/chapter/Chapter.js
@@ -1,8 +1,8 @@
 const Chapter = `
   type Chapter @model {
-    order: Int! @unique
-    title: String! @unique
-    description: String @uniqueOrEmpty
+    order: Int! @unique @length(min: 1, max: 10)
+    title: String! @unique @length(min: 6, max: 120)
+    description: String @uniqueOrEmpty @length(min: 6, max: 120)
     status: ContentStatus! @defaultValue(value: "unpublished")
     createdAt: Date @readOnly
     updatedAt: Date @readOnly
