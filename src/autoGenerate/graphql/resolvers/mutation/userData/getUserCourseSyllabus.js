ae82e0bff106a544f59b826226a5056fe570d0c2
Kritesh Patel <kriteshpk@gmail.com>
2019-03-16 16:17:00 +0530

    placing checks in case of inconsistent data

--- src/autoGenerate/graphql/resolvers/mutation/userData/getUserCourseSyllabus.js
+++ src/autoGenerate/graphql/resolvers/mutation/userData/getUserCourseSyllabus.js
@@ -1,7 +1,7 @@
 import { get } from 'lodash';
 import { operationName } from '../../../../../../constants';
 import { getFieldsBeingFetched } from '../../../../utils';
-import { UnauthenticatedUserError } from '../../../../../../constants/errors';
+import { UnauthenticatedUserError, UnknownUserError } from '../../../../../../constants/errors';
 import { validate } from '../../../validation';
 import callGraphqlApi from '../../../../../api/callGraphqlApi';
 import { ifAuthorized } from '../../../../../../utils';
@@ -23,6 +23,7 @@ const getUserCourseSyllabusMutationResolver = async (
   validate(operationName.read, accessFields, feildsFetched, authentication, {});
   const decodedUser = authentication && authentication.user;
   if (decodedUser && decodedUser.id) {
+    // query to get current compoenent status of user
     const query = `
     query{
       userCurrentComponentStatuses(filter:{
@@ -85,7 +86,6 @@ const getUserCourseSyllabusMutationResolver = async (
     }
     `;
 
-
     const res = await callGraphqlApi(
       query,
       '',
@@ -93,73 +93,99 @@ const getUserCourseSyllabusMutationResolver = async (
       '',
       token,
     );
+    // Ideally each user wull have 1 document in the collection. Fetching the same document
     const currentStatus = get(res, 'data.userCurrentComponentStatuses[0]', null);
-    const {
-      user,
-      currentCourse,
-      currentComponentType: currentComponent,
-      currentTopic,
-      currentLearningObjective } = currentStatus;
-    const currentUserSyllabus = {};
-    const chapters = currentCourse.chapters;
-    for (let i = 0; i < chapters.length;) {
-      for (let j = 0; j < chapters[i].topics.length;) {
-        let isUnlocked = false;
-        if (chapters[i].topics[j].order <= currentTopic.order) { isUnlocked = true; }
-        chapters[i].topics[j].isUnlocked = isUnlocked;
-        j += 1;
+    if (currentStatus) {
+      const {
+        user,
+        currentCourse,
+        currentComponentType: currentComponent,
+        currentTopic,
+        currentLearningObjective,
+      } = currentStatus;
+      // this object will be returned in output
+      const currentUserSyllabus = {};
+      let chapters;
+      if (currentCourse) {
+        chapters = currentCourse.chapters;
       }
-      i += 1;
+      if (chapters.length) {
+        for (let i = 0; i < chapters.length;) {
+          if (chapters[i].topics.length) {
+            for (let j = 0; j < chapters[i].topics.length;) {
+              let isUnlocked = false;
+              if (chapters[i].topics[j].order <= currentTopic.order) {
+                isUnlocked = true;
+              }
+              chapters[i].topics[j].isUnlocked = isUnlocked;
+              j += 1;
+            }
+          }
+          i += 1;
+        }
+      }
+      if (user) { currentUserSyllabus.user = user; }
+      currentUserSyllabus.currentCourse = {};
+      if (currentCourse) {
+        currentUserSyllabus.currentCourse.id = currentCourse.id;
+        currentUserSyllabus.currentCourse.title = currentCourse.title;
+      }
+      currentUserSyllabus.currentComponent = currentComponent;
+      currentUserSyllabus.chapters = chapters;
+      currentUserSyllabus.currentComponentData = {};
+
+      let componentTitle;
+      let thumbnail;
+      let percentageCovered;
+      let description;
+
+      switch (currentComponent) {
+        case 'video':
+          if (currentTopic) {
+            componentTitle = currentTopic.videoTitle;
+            thumbnail = currentTopic.videoThumbnail;
+            percentageCovered = 0;
+            description = currentTopic.videoDescription;
+          }
+          break;
+        case 'chat':
+          if (currentLearningObjective) {
+            componentTitle = currentLearningObjective.title;
+            thumbnail = currentLearningObjective.thumbnail;
+            percentageCovered = 25;
+            description = currentLearningObjective.description;
+          }
+          break;
+        case 'PQ':
+          if (currentLearningObjective) {
+            componentTitle = currentLearningObjective.title;
+            thumbnail = currentLearningObjective.thumbnail;
+            percentageCovered = 50;
+            description = currentLearningObjective.description;
+          }
+          break;
+        case 'quiz':
+          if (currentTopic) {
+            componentTitle = 'Quiz';
+            thumbnail = currentTopic.videoThumbnail;
+            percentageCovered = 75;
+            description = currentTopic.videoDescription;
+          }
+          break;
+        default:
+      }
+
+      currentUserSyllabus.currentComponentData.componentTitle = componentTitle;
+      if (currentTopic) {
+        currentUserSyllabus.currentComponentData.topicTitle = currentTopic.title;
+      }
+      currentUserSyllabus.currentComponentData.thumbnail = thumbnail;
+      currentUserSyllabus.currentComponentData.percentageCovered = percentageCovered;
+      currentUserSyllabus.currentComponentData.description = description;
+
+      return currentUserSyllabus;
     }
-
-    currentUserSyllabus.user = user;
-    currentUserSyllabus.currentCourse = {};
-    currentUserSyllabus.currentCourse.id = currentCourse.id;
-    currentUserSyllabus.currentCourse.title = currentCourse.title;
-    currentUserSyllabus.currentComponent = currentComponent;
-    currentUserSyllabus.chapters = chapters;
-    currentUserSyllabus.currentComponentData = {};
-
-    let componentTitle;
-    let thumbnail;
-    let percentageCovered;
-    let description;
-
-    switch (currentComponent) {
-      case 'video':
-        componentTitle = currentTopic.videoTitle;
-        thumbnail = currentTopic.videoThumbnail;
-        percentageCovered = 0;
-        description = currentTopic.videoDescription;
-        break;
-      case 'chat':
-        componentTitle = currentLearningObjective.title;
-        thumbnail = currentLearningObjective.thumbnail;
-        percentageCovered = 25;
-        description = currentLearningObjective.description;
-        break;
-      case 'PQ':
-        componentTitle = currentLearningObjective.title;
-        thumbnail = currentLearningObjective.thumbnail;
-        percentageCovered = 50;
-        description = currentLearningObjective.description;
-        break;
-      case 'quiz':
-        componentTitle = 'Quiz';
-        thumbnail = currentTopic.videoThumbnail;
-        percentageCovered = 75;
-        description = currentTopic.videoDescription;
-        break;
-      default:
-    }
-
-    currentUserSyllabus.currentComponentData.componentTitle = componentTitle;
-    currentUserSyllabus.currentComponentData.topicTitle = currentTopic.title;
-    currentUserSyllabus.currentComponentData.thumbnail = thumbnail;
-    currentUserSyllabus.currentComponentData.percentageCovered = percentageCovered;
-    currentUserSyllabus.currentComponentData.description = description;
-
-    return currentUserSyllabus;
+    throw new UnknownUserError();
   }
   throw new UnauthenticatedUserError();
 };
