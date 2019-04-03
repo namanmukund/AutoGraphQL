591b3b9fefd3cffe42e2463d1467b3cfa6395750
namanmukund <namanmukund@gmail.com>
2019-04-03 23:15:39 +0530

    schema format

--- config/graphqlSchema/core/types/learningObjective/LearningObjective.js
+++ config/graphqlSchema/core/types/learningObjective/LearningObjective.js
@@ -1,89 +1,51 @@
-import { ADD, META_QUERY, SINGULAR, UPDATE } from '../../../../../constants/graphqlOperations';
+import { ADD, META_QUERY, PLURAL, SINGULAR, UPDATE } from '../../../../../constants/graphqlOperations';
 
 const LearningObjective = `
   type LearningObjective @model 
-  @appPermissions(permissions:[{
-        appName: "tekieTms"
-        operations: ["${ADD}", "${SINGULAR}"]
-  }, {
-        appName: "appTwo"
-        operations: ["${ADD}", "${SINGULAR}"]
-  },  
-  {
-        appName: "three"
-        operations: "*"
-  },
-    {
-        appName: "four"
-        operations: "*"
-  }
-  ], rule: allow)
-  
- @userPermissions(permissions:[{
-        userRole: "admin"
-        operations: ["${ADD}", "${SINGULAR}"]
-        appName: "appTwo" 
-  }, {
-        userRole: "admin"
-        appName: "student"
-        operations: "*"
-  },
-  {
-    userRole: "admin"
-        appName: "student"
-        operations: "*"
-  }
-  ], rule: allow) 
-  
-  
-  @allowedOperations(list:["${ADD}", "${SINGULAR}", "${META_QUERY}", "${UPDATE}"])
+  @appPermissions(permissions:[
+  { appName: "tekieTms" operations: ["${ADD}", "${SINGULAR}", "${PLURAL}"] }, 
+  { appName: "appTwo" operations: ["${ADD}", "${SINGULAR}"] },  
+  { appName: "three" operations: "*" },
+  { appName: "four" operations: "*" }
+  ], 
+  rule: deny)
+ @userPermissions(permissions:[
+  { userRole: "admin" operations: ["${ADD}", "${SINGULAR}"] appName: "appTwo" }, 
+  { userRole: "admin" appName: "student" operations: "*" },
+  { userRole: "admin" appName: "student" operations: "*" }
+  ], 
+  rule: allow) 
+  @allowedOperations(list:["${ADD}", "${SINGULAR}","${PLURAL}", "${META_QUERY}", "${UPDATE}"])
   {
     order: Int! 
     title: String! 
            @unique 
            @length(min: 6, max: 120) 
            @trim
-           @appPermissions(permissions: "*", rule: deny)
+    @appPermissions(permissions: "*", rule: allow)
+    
     description: String 
            @uniqueOrEmpty 
            @unique @length(min: 6, max: 120) 
            @trim
-             @appPermissions(permissions:[{
-        appName: "tekieTms"
-        operations: ["${ADD}", "${SINGULAR}"]
-  }, {
-        appName: "appTwo"
-        operations: ["${ADD}", "${SINGULAR}"]
-  },  
-  {
-        appName: "three"
-        operations: "*"
-  },
-    {
-        appName: "four"
-        operations: "*"
-  }
-  ], rule: allow)
+  @appPermissions(permissions:[
+  { appName: "tekieTms" operations: ["${ADD}", "${SINGULAR}", "${PLURAL}"] }, 
+  { appName: "appTwo" operations: ["${ADD}", "${SINGULAR}"] },  
+  { appName: "three" operations: "*" },
+  { appName: "four" operations: "*" }
+  ], 
+  rule: allow)
   
- @userPermissions(permissions:[{
-        userRole: "admin"
-        operations: ["${ADD}", "${SINGULAR}"]
-        appName: "appTwo" 
-  }, {
-        userRole: "admin"
-        appName: "student"
-        operations: "*"
-  },
-  {
-    userRole: "admin"
-        appName: "student"
-        operations: "*"
-  }
-  ], rule: allow) 
+ @userPermissions(permissions:[
+  { userRole: "admin" operations: ["${ADD}", "${SINGULAR}"] appName: "appTwo" }, 
+  { userRole: "admin" appName: "student" operations: "*" },
+  { userRole: "admin" appName: "student" operations: "*" }
+  ], 
+  rule: allow) 
+  
     videoStartTime: Int
     videoEndTime: Int
     topic: Topic @relation(name: "TopicLearningObjective") 
-           @allowedApps(list:["tekieTms"]) 
     messages: [Message] @relation(name: "LearningObjectiveMessage", isSubset: true)
     questionBank: [QuestionBank] @relation(name: "LearningObjectiveQuestionBank", isSubset: true)
     status: ContentStatus! @defaultValue(value: "unpublished")
