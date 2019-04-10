82f57205a0ca0bb4199bd06be2b62dbd7919d44b
Kritesh Patel <kriteshpk@gmail.com>
2019-04-10 23:01:19 +0530

    changing validation of topic Id in prehook of updateUserCurrentComponentStatus

--- src/autoGenerate/graphql/preHookFunctions/validation/updateUserCurrentTopicComponentStatusValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/updateUserCurrentTopicComponentStatusValidation.js
@@ -34,23 +34,29 @@ const userCurrentTopicComponentStatusQuery = async userCurrentTopicComponentStat
 const updateUserCurrentTopicComponentStatusValidation = async (params) => {
   const { id: userCurrentTopicComponentStatusId } = params;
   const { currentTopicConnectId: topicId } = params;
-  if (!userCurrentTopicComponentStatusId || !topicId) {
+  if (!userCurrentTopicComponentStatusId) {
     throw new TopicOrUserCurrentTopicComponentNotPresentError();
   }
-  // fetching order of the topic called
-  const topicData = await callGraphqlApi(await topicQuery(topicId));
-  const topicOrder = get(topicData, 'data.topic.order');
-  // Fetching userCurrentTopicComponentStatus to get order of current topic
-  const userCurrentTopicComponentStatusData = await callGraphqlApi(
-    await userCurrentTopicComponentStatusQuery(userCurrentTopicComponentStatusId));
-  const userCurrentTopicComponentTopicOrder = get(
-    userCurrentTopicComponentStatusData,
-    'data.userCurrentTopicComponentStatus.currentTopic.order');
+  /*
+  fetching order of the topic called
+  This condtion is placed to check if user is trying to update componentType and not topic
+  In that case this validation will not get fired
+  */
+  if (topicId) {
+    const topicData = await callGraphqlApi(await topicQuery(topicId));
+    const topicOrder = get(topicData, 'data.topic.order');
+    // Fetching userCurrentTopicComponentStatus to get order of current topic
+    const userCurrentTopicComponentStatusData = await callGraphqlApi(
+      await userCurrentTopicComponentStatusQuery(userCurrentTopicComponentStatusId));
+    const userCurrentTopicComponentTopicOrder = get(
+      userCurrentTopicComponentStatusData,
+      'data.userCurrentTopicComponentStatus.currentTopic.order');
     // checking if topic passed order is greater than current topic's
-  if (userCurrentTopicComponentTopicOrder &&
+    if (userCurrentTopicComponentTopicOrder &&
       topicOrder &&
       topicOrder <= userCurrentTopicComponentTopicOrder) {
-    throw new InvalidTopicPassedInCurrentTopicComponent();
+      throw new InvalidTopicPassedInCurrentTopicComponent();
+    }
   }
   return true;
 };
