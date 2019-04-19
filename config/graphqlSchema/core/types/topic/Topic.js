9902e491880a229bc48696df3e3abcda6935da46
Kritesh Patel <kriteshpk@gmail.com>
2019-04-19 12:27:52 +0530

    reverting app permission changes for topic schema

--- config/graphqlSchema/core/types/topic/Topic.js
+++ config/graphqlSchema/core/types/topic/Topic.js
@@ -1,5 +1,3 @@
-import { ADD, PLURAL, SINGULAR } from '../../../../../constants/graphqlOperations';
-
 const Topic = `
   type Topic @model {
     order: Int! @unique @length(min: 1, max: 50)
@@ -17,13 +15,6 @@ const Topic = `
     videoStatus: ContentStatus! @defaultValue(value: "unpublished")
     chapter: Chapter @relation(name: "ChapterTopic")
     learningObjectives: [LearningObjective] @relation(name: "TopicLearningObjective", isSubset: true)
-      @appPermissions(permissions:[
-      { appName: "tekieTms" operations: ["${ADD}", "${SINGULAR}", "${PLURAL}"] }, 
-      { appName: "appTwo" operations: ["${ADD}", "${SINGULAR}"] },  
-      { appName: "three" operations: "*" },
-      { appName: "four" operations: "*" }
-      ], 
-      rule: allow)
     questions: [QuestionBank] @relation(name: "TopicQuestionBank")
     thumbnail: File @relation(name: "TopicThumbnail", direction: "OneWay")
     isTrial: Boolean @defaultValue(value: "false")
