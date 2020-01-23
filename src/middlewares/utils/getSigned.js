5a2fa9d0b1c0799559322040d1563c9a20394a2f
anweshknayak <anweshnayak@Anweshs-MacBook-Air.local>
2020-01-23 22:37:20 +0530

    Added signed url fucntionality with cloudfront integration

--- src/middlewares/utils/getSigned.js
+++ src/middlewares/utils/getSigned.js
@@ -1,8 +1,12 @@
 // eslint-disable-next-line import/no-unresolved
+
 const cf = require('aws-cloudfront-sign');
 import { awsConfig } from '../../../utils/index'
 
-const options = { keypairId: awsConfig.cloudFront.keypairId, privateKeyString: awsConfig.cloudFront.privateKeyString, expireTime: new Date().getTime() + 8000000000 };
+const env = 'cloudFront'
+const aws = awsConfig[env];
+
+const options = { keypairId: aws.keypairId, privateKeyString: aws.privateKeyString, expireTime: aws.expireTime };
 
 const generateSignedUrl = async (url) => {
   const tempUrl = 'https://'+ awsConfig.cloudFront.uri.toString() + "/" + url;
