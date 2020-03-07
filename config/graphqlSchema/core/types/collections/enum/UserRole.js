d422721588f9bac42ed77cd5f2facb5d5ab27f62
namanmukund <namanmukund@gmail.com>
2020-03-08 03:38:12 +0530

    adding mentor and mentee role

--- config/graphqlSchema/core/types/collections/enum/UserRole.js
+++ config/graphqlSchema/core/types/collections/enum/UserRole.js
@@ -1,10 +1,14 @@
-const UserRole = `
-  enum UserRole {
-     student
-     selfLearner
-     contentManager
-     userManager
-     admin
-  }`;
+import { ALL_ROLES_ARRAY } from '../../../../../../constants/roles';
+
+export const getUserRoleEnum = () => {
+  let userRoleEnum = 'enum UserRole {';
+  ALL_ROLES_ARRAY.forEach((role) => {
+    userRoleEnum += `${role} `;
+  });
+  userRoleEnum += '}';
+  return userRoleEnum;
+};
+
+const UserRole = getUserRoleEnum();
 
 export default UserRole;
--- constants/roles.js
+++ constants/roles.js
@@ -3,13 +3,25 @@ const ADMIN = 'admin';
 const SELF_LEARNER = 'selfLearner';
 const STUDENT = 'student';
 const USER_MANAGER = 'userManager';
+const MENTOR = 'mentor';
+const MENTEE = 'mentee';
 
 const CMS_HEAD = `["${ADMIN}", "${CONTENT_MANAGER}"]`;
-const NOT_CMS_HEAD = `["${SELF_LEARNER}", "${STUDENT}", "${USER_MANAGER}"]`;
+const NOT_CMS_HEAD = `["${SELF_LEARNER}", "${STUDENT}", "${USER_MANAGER}", "${MENTOR}", "${MENTEE}"]`;
 const UMS_HEAD = `["${ADMIN}", "${USER_MANAGER}"]`;
-const NOT_UMS_HEAD = `["${CONTENT_MANAGER}", "${SELF_LEARNER}", "${STUDENT}"]`;
-const ALL_ROLES = `["${ADMIN}", "${CONTENT_MANAGER}", "${SELF_LEARNER}", "${STUDENT}", "${USER_MANAGER}"]`;
-const NOT_ADMIN = `["${CONTENT_MANAGER}", "${SELF_LEARNER}", "${STUDENT}", "${USER_MANAGER}"]`;
+const NOT_UMS_HEAD = `["${CONTENT_MANAGER}", "${SELF_LEARNER}", "${STUDENT}", "${MENTOR}", "${MENTEE}"]`;
+const ALL_ROLES = `["${ADMIN}", "${CONTENT_MANAGER}", "${SELF_LEARNER}", "${STUDENT}", "${USER_MANAGER}", "${MENTOR}", "${MENTEE}"]`;
+const NOT_ADMIN = `["${CONTENT_MANAGER}", "${SELF_LEARNER}", "${STUDENT}", "${USER_MANAGER}", "${MENTOR}", "${MENTEE}"]`;
+
+const ALL_ROLES_ARRAY = [
+  CONTENT_MANAGER,
+  ADMIN,
+  SELF_LEARNER,
+  STUDENT,
+  USER_MANAGER,
+  MENTOR,
+  MENTEE,
+];
 
 export {
   CONTENT_MANAGER,
@@ -23,4 +35,7 @@ export {
   NOT_UMS_HEAD,
   ALL_ROLES,
   NOT_ADMIN,
+  MENTOR,
+  MENTEE,
+  ALL_ROLES_ARRAY,
 };
