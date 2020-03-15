74548b031f1d1adfaabd7e24a0611209ebc94321
namanmukund <namanmukund@gmail.com>
2020-03-15 18:15:09 +0530

    mentor token for a specific app is attached in authorization

---
+++ constants/appSpecificAuthTokens.js
@@ -0,0 +1,9 @@
+import { TWA, TLA, TMS } from './index';
+
+const appSpecificAuthTokens = {
+  [TWA]: 'currentMentor',
+  [TLA]: 'currentMentor',
+  [TMS]: 'currentMentor',
+};
+
+export default appSpecificAuthTokens;
--- utils/ifAuthorized.js
+++ utils/ifAuthorized.js
@@ -1,13 +1,31 @@
+import { MENTOR } from '../constants/roles';
+
 export const authenticateUser = (context) => {
-  if (context && context.decodedUser && (context.decodedUser.id || context.decodedUser === true)) {
-    return context.decodedUser;
+  if (context && context.currentUser && (
+    (context.currentUser.id && context.currentUser.status) || context.currentUser === true)) {
+    return context.currentUser;
   }
   return false;
 };
 
 export const authenticateApp = (context) => {
-  if (context && context.decodedApp && (context.decodedApp.name || context.decodedApp === true)) {
-    return context.decodedApp;
+  if (context && context.currentApp && (context.currentApp.name || context.currentApp === true)) {
+    return context.currentApp;
+  }
+  return false;
+};
+
+// custom function built for a specific functionality
+export const authenticateMentor = (context) => {
+  if (context && context.currentMentor
+    && ((
+      context.currentMentor.id
+      && context.currentMentor.role
+      && context.currentMentor.role === MENTOR
+      && context.currentMentor.status
+      && context.currentMentor.status === 'active')
+      || context.currentMentor === true)) {
+    return context.currentMentor;
   }
   return false;
 };
@@ -15,8 +33,13 @@ export const authenticateApp = (context) => {
 export const ifAuthorized = (context) => {
   const app = authenticateApp(context);
   const user = authenticateUser(context);
-  return {
-    app,
-    user,
-  };
+  const mentor = authenticateMentor(context);
+  const obj = {};
+  // no need to show mentor tag inn context if it has no value
+  if (mentor) {
+    obj.mentor = mentor;
+  }
+  obj.app = app;
+  obj.user = user;
+  return obj;
 };
