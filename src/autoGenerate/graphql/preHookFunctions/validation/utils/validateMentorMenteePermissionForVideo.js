6d4d785e4ed2440ff013665c08e6d244b98e35b4
Naman Mukund <namanmukund@gmail.com>
2020-03-17 16:58:54 +0530

    fixing lint error

--- src/autoGenerate/graphql/preHookFunctions/validation/utils/validateMentorMenteePermissionForVideo.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/utils/validateMentorMenteePermissionForVideo.js
@@ -7,22 +7,22 @@ import { topicTypes } from '../../../../../../constants';
 this method validates whether user should be able to hit API on basis of user role
 */
 const validateMentorMenteePermissionForVideo = (context, currentTopicOrder, topicOrder, page, currentTopicComponentType) => {
-    const {
-        video
-    } = topicTypes;
-    const currentUserRole = get(context, 'currentUser.role');
-    const currentMentorId = get(context, 'currentMentor.id');
+  const {
+    video,
+  } = topicTypes;
+  const currentUserRole = get(context, 'currentUser.role');
+  const currentMentorId = get(context, 'currentMentor.id');
 
-    // condition if mentee is trying to access a video which is next to be taught
-    if (currentUserRole === MENTEE &&
-        !currentMentorId &&
-        topicOrder === currentTopicOrder &&
-        page === video &&
-        currentTopicComponentType === video) {
-        throw new InsufficientPermissionError();
-    }
+  // condition if mentee is trying to access a video which is next to be taught
+  if (currentUserRole === MENTEE
+        && !currentMentorId
+        && topicOrder === currentTopicOrder
+        && page === video
+        && currentTopicComponentType === video) {
+    throw new InsufficientPermissionError();
+  }
 
-    return true;
+  return true;
 };
 
 export default validateMentorMenteePermissionForVideo;
