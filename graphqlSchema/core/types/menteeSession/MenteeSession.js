0332d9c9a66f7189a05a00f5ef8edb5a3fcf8982
namanmukund <namanmukund@gmail.com>
2020-04-27 01:53:50 +0530

    course removed form menteeSesion

--- graphqlSchema/core/types/menteeSession/MenteeSession.js
+++ graphqlSchema/core/types/menteeSession/MenteeSession.js
@@ -5,7 +5,6 @@ const slotTimeFields = getSlotTimeFields('Boolean', false);
 const MenteeSession = `
   type MenteeSession @model {
     user: User! @relation(name: "MenteeSessionUser", direction: "OneWay")
-    course: Course! @relation(name: "MenteeSessionCourse", direction: "OneWay")
     topic: Topic! @relation(name: "MenteeSessionTopic", direction: "OneWay")
     mentor: User @relation(name: "MenteeSessionMentor", direction: "OneWay")
     bookingDate: Date!
