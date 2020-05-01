15497621237b4b76adb4c0de4517a066a7922c4a
namanmukund <namanmukund@gmail.com>
2020-05-01 19:41:34 +0530

    available slots permission

--- graphqlSchema/core/types/availableSlot/AvailableSlot.js
+++ graphqlSchema/core/types/availableSlot/AvailableSlot.js
@@ -1,10 +1,16 @@
 import getSlotTimeFields from '../../functions/getSlotTimeFields';
+import { TMS } from '../../../../constants';
+import { READ } from '../../../../constants/graphqlOperations';
 
 const slotTimeFields = getSlotTimeFields('Int', 0);
 
 const AvailableSlot = `
   type AvailableSlot @model 
-  @appPermissions( permissions: "*", rule: deny)
+    @appPermissions(
+    permissions:[
+      { appName: "${TMS}" operations: ${READ} }], 
+    rule: allow
+  ) 
   {
     date: Date
     ${slotTimeFields}
