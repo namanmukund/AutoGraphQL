f5d151a9a0fd1b4dadcd02532e9ea2916f67c9a2
namanmukund <namanmukund@gmail.com>
2020-06-05 20:19:05 +0530

    user permission

--- graphqlSchema/core/types/user/User.js
+++ graphqlSchema/core/types/user/User.js
@@ -1,4 +1,4 @@
-import { ADMIN, NOT_ADMIN } from '../../../../constants/roles';
+import { ADMIN, NOT_ADMIN, UMS_HEAD } from '../../../../constants/roles';
 import { READ } from '../../../../constants/graphqlOperations';
 
 const User = `
@@ -11,7 +11,7 @@ const User = `
     role: UserRole! @defaultValue(value: "selfLearner") 
           @userPermissions(
             permissions:[
-              { userRole: ${ADMIN} appName: "*" operations: "*" },
+              { userRole: ${UMS_HEAD} appName: "*" operations: "*" },
               { userRole: ${NOT_ADMIN} appName: "*" operations: ${READ} }
               ], 
             rule: allow
@@ -22,7 +22,7 @@ const User = `
     savedPassword: String @filterOff 
                  @userPermissions(
                   permissions:[
-                    { userRole: ${ADMIN} appName: "*" operations: "*" }
+                    { userRole: ${UMS_HEAD} appName: "*" operations: "*" }
                     ], 
                   rule: allow
                 )
