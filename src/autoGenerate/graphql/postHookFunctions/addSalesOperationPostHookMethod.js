7a61b62b235360ac13f369bf1a4edbc63059fe99
namanmukund <namanmukund@gmail.com>
2020-06-18 21:43:01 +0530

    code modularity

--- src/autoGenerate/graphql/postHookFunctions/addSalesOperationPostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/addSalesOperationPostHookMethod.js
@@ -1,12 +1,6 @@
 import { ADMIN, UMS_ADMIN, UMS_VIEWER } from '../../../../constants/roles';
 import getReferredByUserIdByAcceptedUserId from '../resolvers/mutation/user/utils/getReferredByUserIdByAcceptedUserId';
-import getNumberOfReferralsOfAUser from '../resolvers/mutation/user/utils/getNumberOfReferralsOfAUser';
-import { MAX_ALLOWED_REFERRALS } from '../../../../constants';
-import addUserCredit from '../resolvers/mutation/user/utils/addUserCredit';
-import referralCredits from '../../../../constants/referralCredits';
-import { log } from '../../../../utils';
-import getAParticularUserInvite from '../resolvers/mutation/user/utils/getAParticularUserInvite';
-import updateAUserInvite from '../resolvers/mutation/user/utils/updateAUserInvite';
+import registrationVerificationAddUpdateUserCredit from './utils/registrationVerificationAddUpdateUserCredit';
 
 const allowedRoles = [ADMIN, UMS_ADMIN, UMS_VIEWER];
 const addSalesOperationPostHookMethod = async (input, params, mutationName, context) => {
@@ -17,27 +11,10 @@ const addSalesOperationPostHookMethod = async (input, params, mutationName, cont
   if ((userVerificationStatus && userVerificationStatus === 'verified') && allowedRoles.includes(role)) {
     const referredByUserId = await getReferredByUserIdByAcceptedUserId(clientConnectId);
     if (referredByUserId) {
-      const numberOfReferralsOfAUser = await getNumberOfReferralsOfAUser(referredByUserId);
-      if (numberOfReferralsOfAUser <= MAX_ALLOWED_REFERRALS) {
-        // add credits
-        const addUserCreditId = await addUserCredit(referralCredits.registrationVerified, referredByUserId);
-        if (addUserCreditId) {
-          const userInviteId = await getAParticularUserInvite(referredByUserId, clientConnectId);
-          if (userInviteId) {
-            const variables = {
-              input: {
-                registrationVerified: true,
-                registrationVerifiedDate: new Date().toISOString(),
-              },
-            };
-            await updateAUserInvite(userInviteId, context, variables);
-          }
-        }
-      } else {
-        log(`Max referral limit exceeded by userId ${referredByUserId}`);
-      }
+      await registrationVerificationAddUpdateUserCredit(referredByUserId, clientConnectId, context);
     }
   }
 };
+
 
 export default addSalesOperationPostHookMethod;
---
+++ src/autoGenerate/graphql/postHookFunctions/utils/registrationVerificationAddUpdateUserCredit.js
@@ -0,0 +1,38 @@
+import getNumberOfReferralsOfAUser from '../../resolvers/mutation/user/utils/getNumberOfReferralsOfAUser';
+import { MAX_ALLOWED_REFERRALS } from '../../../../../constants';
+import getUserCreditId from '../../resolvers/mutation/user/utils/getUserCreditId';
+import updateUserCreditsCount from '../../resolvers/mutation/user/utils/updateUserCreditsCount';
+import referralCredits from '../../../../../constants/referralCredits';
+import addUserCredit from '../../resolvers/mutation/user/utils/addUserCredit';
+import getAParticularUserInvite from '../../resolvers/mutation/user/utils/getAParticularUserInvite';
+import updateAUserInvite from '../../resolvers/mutation/user/utils/updateAUserInvite';
+import { log } from '../../../../../utils';
+
+const registrationVerificationAddUpdateUserCredit = async (referredByUserId, clientConnectId, context) => {
+  const numberOfReferralsOfAUser = await getNumberOfReferralsOfAUser(referredByUserId);
+  if (numberOfReferralsOfAUser <= MAX_ALLOWED_REFERRALS) {
+    // add credits
+    const userCreditId = await getUserCreditId(referredByUserId);
+    // update credit if userCreditId exist else add it
+    if (userCreditId) {
+      await updateUserCreditsCount(referralCredits.registrationVerified, referredByUserId, 'inc');
+    } else {
+      await addUserCredit(referralCredits.registrationVerified, referredByUserId);
+    }
+    // update user credit
+    const userInviteId = await getAParticularUserInvite(referredByUserId, clientConnectId);
+    if (userInviteId) {
+      const variables = {
+        input: {
+          registrationVerified: true,
+          registrationVerifiedDate: new Date().toISOString(),
+        },
+      };
+      await updateAUserInvite(userInviteId, context, variables);
+    }
+  } else {
+    log(`Max referral limit exceeded by userId ${referredByUserId}`);
+  }
+};
+
+export default registrationVerificationAddUpdateUserCredit;
