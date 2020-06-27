db706cf6848284c857afbaf3927062e2171dd115
namanmukund <namanmukund@gmail.com>
2020-06-27 17:03:15 +0530

    from referral

--- src/autoGenerate/graphql/resolvers/mutation/payment/getPaymentResponse.js
+++ src/autoGenerate/graphql/resolvers/mutation/payment/getPaymentResponse.js
@@ -20,7 +20,7 @@ import getEmailObject from '../../../../../../services/email/utils/getEmailObjec
 import sendEmail from '../../../../../../services/email/utils/sendEmail';
 import updateReferrerCreditsPostSessionOrUserPayment from '../../../postHookFunctions/utils/updateReferrerCreditsPostSessionOrUserPayment';
 import referralCredits from '../../../../../../constants/referralCredits';
-import { COURSE_PURCHASED } from '../../../../../../constants/userCreditReason';
+import { COURSE_PURCHASED_FROM_REFERRAL } from '../../../../../../constants/userCreditReason';
 
 // query to get user paayment info for id
 const getUserPayment = (id) => `
@@ -322,7 +322,7 @@ const getPaymentResponseMutationResolver = async (
       };
       const { coursePurchased } = referralCredits[1];
 
-      await updateReferrerCreditsPostSessionOrUserPayment(userId, coursePurchased, context, variables, COURSE_PURCHASED);
+      await updateReferrerCreditsPostSessionOrUserPayment(userId, coursePurchased, context, variables, COURSE_PURCHASED_FROM_REFERRAL);
     } catch (e) {
       log('Error in updateReferrerCreditsPostUserPayment', e);
     }
