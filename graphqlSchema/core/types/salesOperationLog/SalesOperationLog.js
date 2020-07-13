6d9575a66885026ac4cd4bb0ae43077880bfeb33
namanmukund <namanmukund@gmail.com>
2020-07-13 15:28:54 +0530

    permission added

--- graphqlSchema/core/types/salesOperationLog/SalesOperationLog.js
+++ graphqlSchema/core/types/salesOperationLog/SalesOperationLog.js
@@ -1,5 +1,5 @@
 import { TMS } from '../../../../constants';
-import { UMS_HEAD, UMS_VIEWER } from '../../../../constants/roles';
+import { MENTOR, UMS_HEAD, UMS_VIEWER } from '../../../../constants/roles';
 
 const SalesOperationLog = `
   type SalesOperationLog @model
@@ -12,7 +12,8 @@ const SalesOperationLog = `
   @userPermissions(
     permissions:[
       { userRole: ${UMS_HEAD} appName: "*" operations: "*" },
-      { userRole: ${UMS_VIEWER} appName: "*" operations: "*" }
+      { userRole: ${UMS_VIEWER} appName: "*" operations: "*" },
+      { userRole: ${MENTOR} appName: "*" operations: "*" }
       ], 
     rule: allow
   ) 
