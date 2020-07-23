74fbbcae96289007f6db35dbbd6b19dff7dfdf92
namanmukund <namanmukund@gmail.com>
2020-07-23 22:15:43 +0530

    more field added

--- graphqlSchema/core/types/salesOperationLog/SalesOperationLog.js
+++ graphqlSchema/core/types/salesOperationLog/SalesOperationLog.js
@@ -23,6 +23,22 @@ const SalesOperationLog = `
     log: String
     type: SalesOperationLogType
     topic: Topic @relation(name: "SalesOperationLogTopic", direction: "OneWay")
+    potentialLead: Boolean
+    mayConvert: Boolean
+    pricingIssue: Boolean
+    disInterested: Boolean
+    differentStream: Boolean
+    dump: Boolean
+    unfit: Boolean
+    knowCoding: Boolean
+    hasRescheduled: Boolean
+    internetIssue: Boolean
+    zoomIssue: Boolean
+    laptopIssue: Boolean
+    didNotRespond: Boolean
+    powerCut: Boolean
+    turnedUpWithoutConfirming: Boolean
+    highlyInterested: Boolean  
   }
 `;
 
