be178502f1145e7769bc4a50a7455817535107cd
namanmukund <namanmukund@gmail.com>
2020-08-26 13:33:07 +0530

    course purchased state

--- src/autoGenerate/graphql/postHookFunctions/utils/updateReferrerCreditsPostSessionOrUserPayment.js
+++ src/autoGenerate/graphql/postHookFunctions/utils/updateReferrerCreditsPostSessionOrUserPayment.js
@@ -5,6 +5,8 @@ import updateUserCreditsCount from '../../resolvers/mutation/user/utils/updateUs
 import getAParticularUserInvite from '../../resolvers/mutation/user/utils/getAParticularUserInvite';
 import updateAUserInvite from '../../resolvers/mutation/user/utils/updateAUserInvite';
 import { log } from '../../../../../utils';
+import { AFFILIATE, MENTEE } from '../../../../../constants/roles';
+import { COURSE_PURCHASED } from '../../../../../constants/userCreditReason';
 
 const updateReferrerCreditsPostSessionOrUserPayment = async (
   inviteAcceptedByUserId,
@@ -13,12 +15,32 @@ const updateReferrerCreditsPostSessionOrUserPayment = async (
   variables,
   userCreditReason,
 ) => {
-  const referredByUserId = await getReferredByUserIdByAcceptedUserId(inviteAcceptedByUserId);
-  if (referredByUserId) {
-    const numberOfReferralsOfAUser = await getNumberOfReferralsOfAUser(referredByUserId);
-    if (numberOfReferralsOfAUser <= MAX_ALLOWED_REFERRALS) {
+  const referredByUserData = await getReferredByUserIdByAcceptedUserId(inviteAcceptedByUserId);
+
+  if (referredByUserData) {
+    const { id: referredByUserId, role, secondaryRole } = referredByUserData;
+    /*
+    --In case of mentee there is a limit of referrals
+    --In case of affiliate it's not
+     */
+    if (role === MENTEE) {
+      const numberOfReferralsOfAUser = await getNumberOfReferralsOfAUser(referredByUserId);
+      if (numberOfReferralsOfAUser <= MAX_ALLOWED_REFERRALS) {
+        // add credits
+        const userCreditDoc = await updateUserCreditsCount(referralCreditsType, referredByUserId, 'inc', userCreditReason);
+        if (userCreditDoc && userCreditDoc.nModified === 1) {
+          // update user invite trialTaken status & date
+          const userInviteId = await getAParticularUserInvite(referredByUserId, inviteAcceptedByUserId);
+          if (userInviteId) {
+            await updateAUserInvite(userInviteId, context, variables);
+          }
+        }
+      } else {
+        log(`Max referral limit exceeded by userId ${referredByUserId}`);
+      }
+    } else if ((role === AFFILIATE || secondaryRole === AFFILIATE) && userCreditReason === COURSE_PURCHASED) {
       // add credits
-      const userCreditDoc = await updateUserCreditsCount(referralCreditsType, referredByUserId, 'inc', userCreditReason);
+      const userCreditDoc = await updateUserCreditsCount(3000, referredByUserId, 'inc', userCreditReason);
       if (userCreditDoc && userCreditDoc.nModified === 1) {
         // update user invite trialTaken status & date
         const userInviteId = await getAParticularUserInvite(referredByUserId, inviteAcceptedByUserId);
@@ -26,8 +48,6 @@ const updateReferrerCreditsPostSessionOrUserPayment = async (
           await updateAUserInvite(userInviteId, context, variables);
         }
       }
-    } else {
-      log(`Max referral limit exceeded by userId ${referredByUserId}`);
     }
   }
   return null;
