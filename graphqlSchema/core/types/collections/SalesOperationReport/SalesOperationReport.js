04fb3a9949c79fcfca82f78a5bc4b823462b4dfc
namanmukund <namanmukund@gmail.com>
2020-11-15 03:53:51 +0530

    addition in sales report

--- graphqlSchema/core/types/collections/SalesOperationReport/SalesOperationReport.js
+++ graphqlSchema/core/types/collections/SalesOperationReport/SalesOperationReport.js
@@ -5,11 +5,24 @@ const SalesOperationReport = `
     userRegisteredCount: Int
     menteeAllSessionsBookedCount: Int
     menteeFirstSessionBookedCount: Int
+    firstSessionAllottedCount: Int
     firstSessionStartedCount: Int
     firstSessionCompletedCount: Int
+    firstMentorMenteeSessionsCount: Int
+    firstUnAssignedSessions: Int
+    firstCompletedSessionsPercentage: Float
     secondSessionCompletedCount: Int
     allSessionsStartedCount: Int
     allSessionsCompletedCount: Int
+    zoomIssue: Int
+    internetIssue: Int
+    laptopIssue: Int
+    chromeIssue: Int
+    powerCut: Int
+    notResponseAndDidNotTurnUp: Int
+    turnedUpButLeftAbruptly: Int
+    leadNotVerifiedProperly: Int
+    otherReasonForReschedule: Int
  }`;
 
 export default SalesOperationReport;
