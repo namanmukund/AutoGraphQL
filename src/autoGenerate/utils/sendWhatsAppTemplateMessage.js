415edd08cc4c6a91565822d75948ab48f3ba8acf
sanatankc <sanatankc@gmail.com>
2021-02-02 12:45:52 +0530

    lint fix

--- src/autoGenerate/utils/sendWhatsAppTemplateMessage.js
+++ src/autoGenerate/utils/sendWhatsAppTemplateMessage.js
@@ -7,19 +7,18 @@ const sendWhatsAppTemplateMessage = async (
   broadcast_name,
   parameters,
 ) => {
-  if (process.env.NODE_ENV === 'production') {
-    const bodyJson = {
-      template_name,
-      broadcast_name: broadcast_name || 'Tekie',
-      parameters: JSON.stringify(parameters),
-    };
-    const headers = {
-      Authorization: process.env.WATI_SECRET,
-      'Content-Type': 'application/json',
-    };
-    const url = process.env.WATI_API_URL + phoneNumber;
-    return fetch(url, { method: 'POST', headers, body: JSON.stringify(bodyJson) });
-  }
+  if (process.env.NODE_ENV !== 'production') return null;
+  const bodyJson = {
+    template_name,
+    broadcast_name: broadcast_name || 'Tekie',
+    parameters: JSON.stringify(parameters),
+  };
+  const headers = {
+    Authorization: process.env.WATI_SECRET,
+    'Content-Type': 'application/json',
+  };
+  const url = process.env.WATI_API_URL + phoneNumber;
+  return fetch(url, { method: 'POST', headers, body: JSON.stringify(bodyJson) });
 };
 
 export default sendWhatsAppTemplateMessage;
