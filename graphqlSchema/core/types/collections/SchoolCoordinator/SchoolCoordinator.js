48faa17409706581a0d4d6f5a41da9b8f6fd11b5
Naman Mukund <naman@crowdbotics.com>
2021-02-14 20:27:36 +0530

    adding coordinator name

--- graphqlSchema/core/types/collections/SchoolCoordinator/SchoolCoordinator.js
+++ graphqlSchema/core/types/collections/SchoolCoordinator/SchoolCoordinator.js
@@ -3,6 +3,7 @@ const SchoolCoordinator = `
    email: String @uniqueOrEmpty @trim
     phone: Phone @uniqueOrEmpty
     role: SchoolCoordinatorRole
+    name: String
  }`;
 
 export default SchoolCoordinator;
