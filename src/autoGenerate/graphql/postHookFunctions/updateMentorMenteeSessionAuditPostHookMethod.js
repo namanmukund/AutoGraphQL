10ba1eaf940bc614ef72f23b85ad110e212a9d01
Smith Gajjar <smith.gajjar@gmail.com>
2021-03-09 09:12:04 +0530

    fix: added exception when auditor assign query is fired

--- src/autoGenerate/graphql/postHookFunctions/updateMentorMenteeSessionAuditPostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/updateMentorMenteeSessionAuditPostHookMethod.js
@@ -19,8 +19,13 @@ const updateMentorMenteeSessionAudit = async (mentorMenteeSessionAuditId, input)
   return get(res, 'data.updateMentorMenteeSessionAudit');
 };
 
-const updateMentorMenteeSessionAuditPostHookMethod = async (input) => {
-  if (get(input, 'status', false) !== 'started') {
+const updateMentorMenteeSessionAuditPostHookMethod = async (input, _mutationName, _context, params) => {
+  const { auditorConnectId } = params;
+  /**
+   * Check if prev status is not started and auditorConnectId not provided then update status to started
+   * i.e To avoid changing status to started when the query for assigning the Auditor is fired.
+   * */
+  if (get(input, 'status', false) !== 'started' && !auditorConnectId) {
     const updateMentorMenteeSessionAuditData = await updateMentorMenteeSessionAudit(get(input, 'id'), { status: 'started' });
     if (updateMentorMenteeSessionAuditData && updateMentorMenteeSessionAuditData.id) {
       Object.assign(input, {
