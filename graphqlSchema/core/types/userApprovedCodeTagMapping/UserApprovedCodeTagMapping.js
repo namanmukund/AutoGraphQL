87928f79ff117fa54c0385aaef25fabcb3083dc1
Smith Gajjar <smith.gajjar@gmail.com>
2021-03-28 14:34:10 +0530

    addition: Title groupBy

--- graphqlSchema/core/types/userApprovedCodeTagMapping/UserApprovedCodeTagMapping.js
+++ graphqlSchema/core/types/userApprovedCodeTagMapping/UserApprovedCodeTagMapping.js
@@ -2,6 +2,7 @@ const UserApprovedCodeTagMapping = `
   type UserApprovedCodeTagMapping @model
   @userToken(isRequired:"false")  
   {
+    title: String @groupBy
     userApprovedCode: UserApprovedCode! @relation(name: "UserApprovedCodeTagMappingCode")
     userApprovedCodeTag: UserApprovedCodeTag! @relation(name: "UserApprovedCodeTagMappingTag", direction: "OneWay") 
   }
