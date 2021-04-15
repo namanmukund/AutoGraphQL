2a2390a610e3d3829dc5194c90c6daca747826c3
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-04-15 18:42:49 +0530

    added:query for cheatSheetId

--- src/autoGenerate/graphql/resolvers/query/methods/getCheatSheet.js
+++ src/autoGenerate/graphql/resolvers/query/methods/getCheatSheet.js
@@ -63,31 +63,61 @@ const getCheatSheet = (async (root, params, context) => {
   let filter = '';
   const { input } = params;
   if (input) {
-    if (input.topicId) {
-      filter = `{topic_some:{id:"${input.topicId}"}}, `;
-    }
-    if (input.cheatSheetId) {
-      filter = `{ id:"${input.cheatSheetId}" }, `;
-    }
-    if (input.searchText) {
-      filter = `{title_contains:"${input.searchText}"}`;
-    }
-    if (input.isFavourite) {
-      filter = '';
-    }
-    const data = await callLocalGraphqlApi(
-      getCheatSheets(filter),
-      context,
-      '',
-    );
-    const cheatSheets = get(data, 'data.cheatSheets', []);
+    let data;
     const cheatSheetArray = [];
-    cheatSheets.forEach((concept, i) => {
-      cheatSheetArray.push({ cheatsheet: { type: 'CheatSheet', typeId: `${concept.id}` }, isSelected: i === 0 });
-    });
-    return {
-      cheatSheetConcepts: [...cheatSheetArray],
-    };
+    if (input.topicId || input.searchText) {
+      if (input.topicId) {
+        filter = `{topic_some:{id:"${input.topicId}"}}, `;
+      }
+      if (input.searchText) {
+        filter = `{title_contains:"${input.searchText}"}`;
+      }
+      data = await callLocalGraphqlApi(
+        getCheatSheets(filter),
+        context,
+        '',
+      );
+      const cheatSheets = get(data, 'data.cheatSheets', []);
+      cheatSheets.forEach((concept, i) => {
+        cheatSheetArray.push({ cheatsheet: { type: 'CheatSheet', typeId: `${concept.id}` }, isSelected: i === 0 });
+      });
+      return {
+        cheatSheetConcepts: [...cheatSheetArray],
+      };
+      /* eslint-disable no-else-return */
+    } else if (input.cheatSheetId) {
+      const topicsData = await callLocalGraphqlApi(
+        getTopics(),
+        context,
+        '',
+      );
+      const topics = get(topicsData, 'data.topics', []);
+      const topicsArray = [];
+      filter = `{ id:"${input.cheatSheetId}" }, `;
+      const cheatsheet = await callLocalGraphqlApi(
+        getCheatSheets(filter),
+        context,
+        '',
+      );
+      const cheatTopic = get(cheatsheet, 'data.cheatSheets[0].topic.id', '');
+      // by default selecting the topic which contains the cheatsheet for which the cheatsheetId is passed in input
+      topics.forEach((topic) => {
+        topicsArray.push({ topic: { type: 'Topic', typeId: `${topic.id}` }, isSelected: get(topic, 'id') === cheatTopic });
+      });
+      data = await callLocalGraphqlApi(
+        getCheatSheets(`{topic_some:{id:"${cheatTopic}"}}`),
+        context,
+        '',
+      );
+      const cheatSheets = get(data, 'data.cheatSheets', []);
+      cheatSheets.forEach((concept, i) => {
+        cheatSheetArray.push({ cheatsheet: { type: 'CheatSheet', typeId: `${concept.id}` }, isSelected: i === 0 });
+      });
+      return {
+        cheatSheetTopics: [...topicsArray],
+        cheatSheetConcepts: [...cheatSheetArray],
+      };
+    }
   }
   const topicsData = await callLocalGraphqlApi(
     getTopics(),
@@ -99,8 +129,21 @@ const getCheatSheet = (async (root, params, context) => {
   topics.forEach((topic, i) => {
     topicsArray.push({ topic: { type: 'Topic', typeId: `${topic.id}` }, isSelected: i === 0 });
   });
+  const cheatSheetArray = [];
+  if (topics.length > 0) {
+    const data = await callLocalGraphqlApi(
+      getCheatSheets(`{topic_some:{id:"${get(topics[0], 'id')}"}}`),
+      context,
+      '',
+    );
+    const cheatsheets = get(data, 'data.cheatSheets', []);
+    cheatsheets.forEach((concept, i) => {
+      cheatSheetArray.push({ cheatsheet: { type: 'CheatSheet', typeId: `${concept.id}` }, isSelected: i === 0 });
+    });
+  }
   return {
     cheatSheetTopics: [...topicsArray],
+    cheatSheetConcepts: [...cheatSheetArray],
   };
 });
 
