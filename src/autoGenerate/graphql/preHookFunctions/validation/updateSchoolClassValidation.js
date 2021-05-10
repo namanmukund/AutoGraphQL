a0aff523b70e524041569a09d8a6abc0a687166f
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-05-10 14:43:01 +0530

    refactoring

--- src/autoGenerate/graphql/preHookFunctions/validation/updateSchoolClassValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/updateSchoolClassValidation.js
@@ -1,70 +1,15 @@
-import { get } from 'lodash';
 import { GradeSectionCombinationAlreadyExists } from '../../../../../constants/errors';
-import callLocalGraphqlApi from '../../../../api/callLocalGraphqlApi';
-
-const getGradeFilter = (grade) => {
-  if (grade) {
-    return `{grade : ${grade}}`;
-  }
-  return '';
-};
-
-const getSectionFilter = (section) => {
-  if (section) {
-    return `{section : ${section}}`;
-  }
-  return '';
-};
-
-const getSchoolFilter = (schoolConnectId) => {
-  if (schoolConnectId) {
-    return `{school_some: {id : "${schoolConnectId}"}}`;
-  }
-  return '';
-};
-
-const getSchoolClassIdFilter = (schoolClassId) => {
-  if (schoolClassId) {
-    return `{ id: "${schoolClassId}" }`;
-  }
-  return '';
-};
-
-const fetchSchoolClass = async (schoolClassId, grade, section, schoolConnectId) => {
-  const query = `
-          {
-            schoolClasses(filter:
-              {
-                and:[
-                  ${getSchoolClassIdFilter(schoolClassId)}
-                  ${getGradeFilter(grade)}
-                  ${getSectionFilter(section)}
-                  ${getSchoolFilter(schoolConnectId)}
-                ]
-              }
-            ){
-              id
-              grade
-              section
-              school{
-                id
-              }
-            }
-          }
-          `;
-  const schoolClass = await callLocalGraphqlApi(query);
-  return get(schoolClass, 'data.schoolClasses', []);
-};
+import fetchSchoolClasses from './utils/checkIfGradeSectionExists';
 
 const updateSchoolClassValidation = async (params) => {
   const { id: schoolClassId, input: { grade, section, schoolConnectId } } = params;
 
   // if update query is to change the school linked to the class, we have to pass the schoolId, and fetch the grade and sections of all classes corresponding to that schoolId
   if (schoolConnectId) {
-    const schoolClassesOfGivenSchoolId = await fetchSchoolClass(null, null, null, schoolConnectId);
+    const schoolClassesOfGivenSchoolId = await fetchSchoolClasses(null, null, null, schoolConnectId);
     if (schoolClassesOfGivenSchoolId && schoolClassesOfGivenSchoolId.length > 0) {
       // then we fetch the already stored grade and section from the given schoolClassId
-      const schoolClassOfGivenId = await fetchSchoolClass(schoolClassId);
+      const schoolClassOfGivenId = await fetchSchoolClasses(schoolClassId);
       if (schoolClassOfGivenId && schoolClassOfGivenId.length > 0) {
         const gradeOfClass = schoolClassOfGivenId[0].grade;
         const sectionOfClass = schoolClassOfGivenId[0].section;
@@ -80,7 +25,7 @@ const updateSchoolClassValidation = async (params) => {
     }
   } else {
     // else if the update query is to change the grade or section of school class without changin the school it is connected to, we first query the school id corresponding to the schoolClassId
-    const schoolClassOfGivenId = await fetchSchoolClass(schoolClassId);
+    const schoolClassOfGivenId = await fetchSchoolClasses(schoolClassId);
     if (schoolClassOfGivenId && schoolClassOfGivenId.length > 0) {
       const schoolId = schoolClassOfGivenId[0].school.id;
       const gradeFetched = schoolClassOfGivenId[0].grade;
@@ -89,13 +34,13 @@ const updateSchoolClassValidation = async (params) => {
       // then we check if for the given input of grade and class if there exists some document in the same school, by passing the school id
 
       if (grade && section) {
-        const schoolClasses = await fetchSchoolClass(null, grade, section, schoolId);
+        const schoolClasses = await fetchSchoolClasses(null, grade, section, schoolId);
         if (schoolClasses && schoolClasses.length > 0) {
           throw new GradeSectionCombinationAlreadyExists();
         }
       } else if (grade && !section) {
         if (sectionFetched) {
-          const schoolClasses = await fetchSchoolClass(null, grade, sectionFetched, schoolId);
+          const schoolClasses = await fetchSchoolClasses(null, grade, sectionFetched, schoolId);
           if (schoolClasses && schoolClasses.length > 0) {
             throw new GradeSectionCombinationAlreadyExists();
           }
@@ -103,7 +48,7 @@ const updateSchoolClassValidation = async (params) => {
       } else if (!grade && section) {
         // grade mandatory but added for uniformity
         if (gradeFetched) {
-          const schoolClasses = await fetchSchoolClass(null, gradeFetched, section, schoolId);
+          const schoolClasses = await fetchSchoolClasses(null, gradeFetched, section, schoolId);
           if (schoolClasses && schoolClasses.length > 0) {
             throw new GradeSectionCombinationAlreadyExists();
           }
