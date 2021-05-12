dadaa31243150c5e9a4b51ff716b2cab6d03371f
Kritesh Patel <kriteshpk@gmail.com>
2021-05-12 17:08:48 +0530

    check for topicId before starting session

--- src/autoGenerate/graphql/preHookFunctions/validation/updateBatchSessionValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/updateBatchSessionValidation.js
@@ -2,11 +2,15 @@ import { get } from 'lodash';
 import callLocalGraphqlApi from '../../../../api/callLocalGraphqlApi';
 import { DatabaseRecordNotFoundError } from '../../../../../constants/errors';
 import batchSessionQuery from '../../graphqlQueries/batchSessionQuery';
-import { CanNotChangeSessionStatusError } from '../../../../../constants/errors/input';
+import {
+  CanNotChangeSessionStatusError,
+  MissingMandatoryInputInRequestError,
+} from '../../../../../constants/errors/input';
 import getSelectedSlotsTime from './utils/getSelectedSlotsTime';
+import { sessionStatus } from '../../../../../constants';
 
 const updateBatchSessionValidation = async (params, mutationOrQueryName, context) => {
-  const { id: batchSessionId, input: { sessionStatus, bookingDate: bookingDateFromInput } } = params;
+  const { id: batchSessionId, topicConnectId, input: { sessionStatus: sessionStatusInInput, bookingDate: bookingDateFromInput } } = params;
   const batchSessionData = await callLocalGraphqlApi(batchSessionQuery(batchSessionId));
   const batchSession = get(batchSessionData, 'data.batchSession');
   if (!batchSession || !batchSession.id) {
@@ -33,8 +37,24 @@ const updateBatchSessionValidation = async (params, mutationOrQueryName, context
   context.slotTimeArray = slotTimeArray;
   context.bookingDateFromInput = bookingDateFromInput;
 
+  // we are doing this to handle cases where we make timetable for school without the topic being attached
+  // so whenever these sessions get started we need topicId in this mutation as mandatory field
+  // if topic id is not in document, we need topicConnectId in input in case session is getting started/completed
+  // throwing error if above criteria is not met else updating topicId in context
+  if ((!topic || (topic && !topic.id)) && (sessionStatusInInput === sessionStatus.started || sessionStatusInInput === sessionStatus.completed)) {
+    if (topicConnectId) {
+      context.topicId = topicConnectId;
+    } else {
+      throw new MissingMandatoryInputInRequestError({
+        data: {
+          message: 'topicConnectId is mandatory in input ',
+        },
+      });
+    }
+  }
+
   // if session is complete and user is trying to change the status then throw error
-  if (prevSessionStatus === 'completed' && sessionStatus && sessionStatus !== 'completed') {
+  if (prevSessionStatus === sessionStatus.completed && sessionStatusInInput && sessionStatusInInput !== sessionStatus.completed) {
     throw new CanNotChangeSessionStatusError();
   }
   return true;
