d1344849c83b37957a74cdf58a2ce57d6ca926d6
Kritesh Patel <kriteshpk@gmail.com>
2021-05-15 17:19:01 +0530

    courseId not mandatory while adding mentorSession

--- src/autoGenerate/graphql/preHookFunctions/validation/addMentorSessionValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/addMentorSessionValidation.js
@@ -51,14 +51,15 @@ const addMentorSessionValidation = async (params, mutationOrQueryName, context) 
   validateMentorSessionInput(params, '', context);
   // check if the document for called user and availabilityDate is already present
   const userId = get(params, 'userConnectId');
-  const courseId = get(params, 'courseConnectId');
+  // courseId not mandatory for mentorSession
+  // const courseId = get(params, 'courseConnectId');
   const availabilityDate = get(params, 'input.availabilityDate');
 
   // log in case user id or availabilityDate is not present
-  if (!userId || !availabilityDate || !courseId) {
+  if (!userId || !availabilityDate) {
     throw new MissingMandatoryInputInRequestError({
       data: {
-        message: 'Either userConnectId or courseConnectId or availabilityDate or all missing in input',
+        message: 'Either userConnectId or availabilityDate or all missing in input',
       },
     });
   }
