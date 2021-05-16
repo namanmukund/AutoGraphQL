736b94b413fa156027f1719d0dc224ab75024c16
namanmukund <namanmukund@gmail.com>
2021-05-16 13:42:47 +0530

    addStudentProfile

--- src/autoGenerate/graphql/postHookFunctions/updateStudentProfilePostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/updateStudentProfilePostHookMethod.js
@@ -1,5 +1,6 @@
 import { get } from 'lodash';
 import callLocalGraphqlApi from '../../../api/callLocalGraphqlApi';
+import addUpdateSchoolClass from './utils/addUpdateSchoolClass';
 
 const updateUserApprovedCodeQuery = async (userApprovedCodeID, input) => {
   const query = `
@@ -34,79 +35,6 @@ const userApprovedCodeQuery = async (userId) => {
   return get(res, 'data.userApprovedCodes');
 };
 
-const getSchoolClass = async (input, studentSchoolId) => {
-  const {
-    grade,
-    section,
-  } = input;
-  const query = `
-  query{
-  schoolClasses(filter:{
-    and:[
-      {school_some:{id:"${studentSchoolId}"}}
-      {grade:${grade}}
-      {section: ${section}}
-    ]
-  }){
-    id
-  }
-}
-  `;
-  const res = await callLocalGraphqlApi(query);
-  return get(res, 'data.schoolClasses.0.id');
-};
-
-const addSchoolClass = async (input, studentSchoolId, studentProfileId) => {
-  const query = `
-    mutation($input: SchoolClassInput!){
-      addSchoolClass(
-      input:$input, 
-      schoolConnectId:"${studentSchoolId}"
-      studentsConnectIds:["${studentProfileId}"]
-      ){
-        id
-      }
-    }
-  `;
-  const { grade, section } = input;
-  const variables = {
-    input: {
-      grade,
-      section,
-    },
-  };
-  const res = await callLocalGraphqlApi(query, '', variables);
-  return get(res, 'data.addSchoolClass.id');
-};
-
-const connectSchoolClassWithStudentProfile = async (schoolClassId, studentSchoolId, studentProfileId) => {
-  const query = `
-    mutation($input: SchoolClassUpdate){
-      updateSchoolClass(
-      id:"${schoolClassId}"
-      input:$input, 
-      studentsConnectIds:["${studentProfileId}"]
-      ){
-        id
-      }
-    }
-  `;
-  const variables = {
-    input: {},
-  };
-  const res = await callLocalGraphqlApi(query, '', variables);
-  return get(res, 'data.updateSchoolClass.id');
-};
-
-const addUpdateSchoolClass = async (input, studentSchoolId, studentProfileId) => {
-  const schoolClassId = await getSchoolClass(input, studentSchoolId);
-  // map student profile with school class if exist else create first and then map
-  if (schoolClassId) {
-    // just connect school class and student profile
-    return connectSchoolClassWithStudentProfile(schoolClassId, studentSchoolId, studentProfileId);
-  }
-  return addSchoolClass(input, studentSchoolId);
-};
 const removeFromSchoolCLassStudentProfile = async (schoolClassId, studentProfileId) => {
   const query = `
   mutation{
@@ -155,7 +83,6 @@ const updateStudentProfilePostHookMethod = async (input, params, mutationName, c
   const currentGrade = input.grade;
   const schoolId = get(input, 'school.typeId');
   const previousSchoolClassId = get(input, 'schoolClass.typeId');
-
   //  section added
   if (!previousSection && currentSection) {
     if (schoolId) {
