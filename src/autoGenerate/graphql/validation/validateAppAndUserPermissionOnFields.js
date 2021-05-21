2f74934f1d5c0c16e9c2c079b7342f97b7335cea
namanmukund <namanmukund@gmail.com>
2021-05-21 17:41:04 +0530

    permission

--- src/autoGenerate/graphql/validation/validateAppAndUserPermissionOnFields.js
+++ src/autoGenerate/graphql/validation/validateAppAndUserPermissionOnFields.js
@@ -1,6 +1,6 @@
 /* eslint no-restricted-syntax: ["error", "FunctionExpression", "WithStatement",
 "BinaryExpression[operator='in']"] */
-import { findIndex } from 'lodash';
+import { findIndex, get } from 'lodash';
 import { InsufficientPermissionError } from '../../../../constants/errors';
 import { META } from '../../../../constants';
 import { SINGULAR } from '../../../../constants/graphqlOperations';
