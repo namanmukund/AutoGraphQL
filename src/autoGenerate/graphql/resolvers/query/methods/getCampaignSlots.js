7327d139d8e725a456ab787c5d4896dfa4196021
sanatankc <sanatankc@gmail.com>
2021-05-23 19:34:33 +0530

    added title

--- src/autoGenerate/graphql/resolvers/query/methods/getCampaignSlots.js
+++ src/autoGenerate/graphql/resolvers/query/methods/getCampaignSlots.js
@@ -11,6 +11,7 @@ const getCampaign = (code) => `
   campaigns(filter: {code: "${code}"}){
     id
     type
+    title
     poster {
       id
     }
@@ -101,6 +102,7 @@ const getCampaignSlots = (async (root, params, context) => {
   const schoolName = get(getCampaignRes, 'data.campaigns[0].school.name', '');
   const schoolLogoId = get(getCampaignRes, 'data.campaigns[0].school.logo.id', '');
   const posterId = get(getCampaignRes, 'data.campaigns[0].poster.id', '');
+  const title = get(getCampaignRes, 'data.campaigns[0].title', '');
   const classes = get(getCampaignRes, 'data.campaigns[0].classes', []);
   const batches = get(getCampaignRes, 'data.campaigns[0].batches', []);
   // by default taking value as 1 in worst case
@@ -118,6 +120,7 @@ const getCampaignSlots = (async (root, params, context) => {
   result.schoolName = schoolName;
   result.classes = classes;
   result.schoolId = schoolId;
+  result.title = title;
   if (schoolLogoId) {
     result.schoolLogo = { type: 'File', typeId: `${schoolLogoId}` };
   }
