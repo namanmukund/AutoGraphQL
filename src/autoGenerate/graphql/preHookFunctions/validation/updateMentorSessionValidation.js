727107bd8df8fca6c9fa04e97000c5d56d538ecc
Kritesh Patel <kriteshpk@gmail.com>
2021-05-25 16:24:02 +0530

    update mentorSession fix

--- src/autoGenerate/graphql/preHookFunctions/validation/updateMentorSessionValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/updateMentorSessionValidation.js
@@ -5,7 +5,9 @@ import mentorSessionQuery from '../../graphqlQueries/mentorSessionQuery';
 import { DatabaseRecordNotFoundError } from '../../../../../constants/errors';
 import getUserIdandAppNameAfterValidation from './utils/getUserIdandAppNameAfterValidation';
 import getSlotTimesInString from '../../../../../utils/getSlotTimesInString';
+import getSelectedSlotsTime from './utils/getSelectedSlotsTime';
 import checkIfSlotCanBeOpenedValidation from './utils/checkIfSlotCanBeOpenedValidation';
+import checkIfSlotCanBeDeletedValidation from './utils/checkIfSlotCanBeDeletedValidation';
 
 // query to get mentor Sessions
 const getMentorSessions = (userId, availabilityDate) => `query{
@@ -60,11 +62,14 @@ const updateMentorSessionValidation = async (params, mutationOrQueryName, contex
         availabilityDate,
       ),
     );
-
+    // if a slot is true from before we do not need to validate that
+    const timeSlotsInPrevDoc = getSelectedSlotsTime(mentorSession);
     // there can be a max of 3 mentorSessions for an availability date of type(batch/trial/paid)
     // we will check that slot which is being sent true in not already booked for other type
     const mentorSessions = get(getMentorSessionsRes, 'data.mentorSessions');
-    checkIfSlotCanBeOpenedValidation(params, mentorSessions);
+    checkIfSlotCanBeOpenedValidation(params, mentorSessions, timeSlotsInPrevDoc);
+    // slots that are already consumed can not be deleted(set to false)
+    checkIfSlotCanBeDeletedValidation(params, mentorSession);
   }
   return true;
 };
