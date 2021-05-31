6ae9fc06d36336662e2852e84d85ace7b73eed55
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-05-31 12:54:23 +0530

    posthook updates

--- src/autoGenerate/graphql/postHookFunctions/removeFromBatchStudentProfile.js
+++ src/autoGenerate/graphql/postHookFunctions/removeFromBatchStudentProfile.js
@@ -1,7 +1,14 @@
+import { get } from 'lodash';
+import { sessionStatus } from '../../../../constants';
 import {
   removeStudentFromBatchSessionAttendance,
   fetchAllottedBatchSessions,
+  fetchStudentProfile,
+  fetchUserCurrentTopicComponentStatuses,
+  fetchNextTopicId,
+  updateUserCurrentTopicComponentStatus,
 } from './utils/removeFromBatchStudentProfileHelperMethods';
+
 /*
   Post hook of remove from batch student profile
 */
@@ -17,6 +24,33 @@ const removeFromBatchStudentProfilePosthookMethod = async (input, params, mutati
       removeStudentFromBatchSessionAttendance(batchSession, studentProfileId);
     }
   }
+  // update userCurrTopicComponent to BatchCurrTopicComponent data
+  // fetch student profile which provide current component status
+  const studentProfiles = await fetchStudentProfile(studentProfileId, batchId);
+  const studentProfile = studentProfiles && studentProfiles[0];
+
+  // fetch the userCurrentComponent from the userId
+  const userId = get(studentProfile, 'user.id', '');
+  const userCurrentComponent = await fetchUserCurrentTopicComponentStatuses(userId);
+
+  // proceed to update only if the current topic in batch is greater than current topic in user
+  const batchCurrentTopicOrder = get(studentProfile, 'batch.currentComponent.currentTopic.order', '');
+  const userCurrentTopicOrder = get(userCurrentComponent, 'currentTopic.order', '');
+  const userCurrentComponentId = get(userCurrentComponent, 'id', '');
+
+  if (batchCurrentTopicOrder > userCurrentTopicOrder) {
+    // check latest session status and update accordingly
+    const latestSessionStatus = get(studentProfile, 'batch.currentComponent.latestSessionStatus', '');
+    if (latestSessionStatus !== sessionStatus.completed) {
+      const batchCurrentTopicId = get(studentProfile, 'batch.currentComponent.currentTopic.id', '');
+      await updateUserCurrentTopicComponentStatus(userCurrentComponentId, batchCurrentTopicId);
+    } else {
+      const topics = await fetchNextTopicId(batchCurrentTopicOrder + 1);
+      const nextTopicId = get(topics[0], 'id');
+      await updateUserCurrentTopicComponentStatus(userCurrentComponentId, nextTopicId);
+    }
+  }
+
 };
 
 export default removeFromBatchStudentProfilePosthookMethod;
--- src/autoGenerate/graphql/postHookFunctions/utils/removeFromBatchStudentProfileHelperMethods.js
+++ src/autoGenerate/graphql/postHookFunctions/utils/removeFromBatchStudentProfileHelperMethods.js
@@ -1,6 +1,98 @@
 import { get } from 'lodash';
 import callLocalGraphqlApi from '../../../../api/callLocalGraphqlApi';
 
+const fetchStudentProfile = async (studentProfileId, batchId) => {
+  const query = `
+    {
+      studentProfiles(filter:
+      {
+        and: [
+          {id: "${studentProfileId}"},
+          {batch_some: {id: "${batchId}"}},
+        ]
+      }
+      ){
+        id
+        batch{
+          id
+          currentComponent{
+            id
+            currentCourse{
+              id
+            }
+            currentTopic{
+              order
+            }
+            currentLearningObjective{
+              order
+            }
+            latestSessionStatus
+          }
+        }
+      }
+    }
+  `;
+  const studentProfiles = await callLocalGraphqlApi(query);
+  return get(studentProfiles, 'data.batchSessions', []);
+}
+
+const fetchUserCurrentTopicComponentStatuses = async (userId) => {
+  const query = `
+    {
+    userCurrentTopicComponentStatuses(
+      filter: {user_some:{id:"${userId}"}}
+    ){
+      id
+      currentCourse{
+        id
+      }
+      currentTopic{
+        order
+      }
+      currentLearningObjective{
+        order
+      }
+    }
+  }
+  `;
+  const currentTopicComponent = await callLocalGraphqlApi(query);
+  return get(currentTopicComponent, 'data.userCurrentTopicComponentStatuses', []);
+}
+
+const fetchNextTopicId = async (topicOrder) => {
+  const query = `
+    {
+      topics(filter: {
+        and:[
+          {order: 4}
+        ]
+      }){
+        id
+        order
+      }
+    }
+  `;
+  const topics = await callLocalGraphqlApi(query);
+  return get(topics, 'data.topics', []);
+}
+
+const updateUserCurrentTopicComponentStatus = async (userCurrentComponentId, topicId) => {
+  const mutation = `
+    mutation{
+    updateUserCurrentTopicComponentStatus(id: "${userCurrentComponentId}"
+      currentTopicConnectId: "${topicId}",
+    ){
+      id
+      currentLearningObjective{
+        order
+      }
+    }
+  }
+  `;
+  const updateResult = await callLocalGraphqlApi(mutation);
+  return get(updateResult, 'data.updateUserCurrentTopicComponentStatus', {});
+}
+
 const fetchAllottedBatchSessions = async (batchId) => {
   const query = `
           {
@@ -79,4 +171,11 @@ const removeStudentFromBatchSessionAttendance = async (batchSession, studentProf
   }
 };
 
-export { fetchAllottedBatchSessions, removeStudentFromBatchSessionAttendance };
+export {
+  fetchAllottedBatchSessions,
+  removeStudentFromBatchSessionAttendance,
+  fetchStudentProfile,
+  fetchUserCurrentTopicComponentStatuses,
+  fetchNextTopicId,
+  updateUserCurrentTopicComponentStatus,
+};
