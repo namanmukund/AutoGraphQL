fde8c11a0b003abf25b30be84bcaa5342ded51f8
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-06-05 18:28:02 +0530

    .

--- src/autoGenerate/graphql/resolvers/query/scriptMethods/updateBatchInMentorProfile.js
+++ src/autoGenerate/graphql/resolvers/query/scriptMethods/updateBatchInMentorProfile.js
@@ -1,6 +1,38 @@
-// import { get } from 'lodash';
+import { get } from 'lodash';
+import callLocalGraphqlApi from '../../../../../api/callLocalGraphqlApi';
+
+const fetchBatchesWithMentorProfiles = async () => {
+  const query = `
+          {
+            batches(filter: {allottedMentor_some:{
+              mentorProfile_exists: true
+            }},
+            first:1000
+            ){
+              id
+              allottedMentor{
+                mentorProfile{
+                  id
+                }
+              }
+            }
+          }
+          `;
+  const batches = await callLocalGraphqlApi(query);
+  return get(batches, 'data.batches', []);
+};
+
 
 const updateBatchInMentorProfileScript = async () => {
-  // console.log('Hi');
+  // fetch from batches, which mentorProfile it belongs to and then update all of those mentor profile with given batchIds.
+  // from batch.allottedMentor.user.mentorProfile.id
+  const batches = await fetchBatchesWithMentorProfiles();
+  const batchesLength = batches.length;
+
+  for (const batch of batches) {
+    console.log(batch);
+  }
+
+
 };
 export default updateBatchInMentorProfileScript;
