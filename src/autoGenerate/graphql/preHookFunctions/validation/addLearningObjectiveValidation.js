2731ae0909627c983e8681dcaf82266d1f272a92
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-06-11 19:05:10 +0530

    removed check for topicID

--- src/autoGenerate/graphql/preHookFunctions/validation/addLearningObjectiveValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/addLearningObjectiveValidation.js
@@ -3,34 +3,36 @@
 import { get } from 'lodash';
 import {
   OrderAlreadyExistsError,
-  TopicIdRequiredError,
 } from '../../../../../constants/errors';
 import callLocalGraphqlApi from '../../../../api/callLocalGraphqlApi';
 
 const addLearningObjectiveValidation = async (params) => {
   const { topicConnectId: id } = params;
-  if (!id) {
-    throw new TopicIdRequiredError();
-  }
-  const { order } = params.input;
-  const query = `
-  query{
-  topic(id:"${id}"){
-    learningObjectives{
-      order
-    }
-  }
- }
-`;
-
-  const res = await callLocalGraphqlApi(query);
-  const learningObjectives = get(res, 'data.topic.learningObjectives');
-  if (learningObjectives) {
-    for (const learningObjective of learningObjectives) {
-      if (learningObjective.order === order) {
-        throw new OrderAlreadyExistsError();
+  // if (!id) {
+  //   throw new TopicIdRequiredError();
+  // }
+  if (id) {
+    const { order } = params.input;
+    const query = `
+    query{
+    topic(id:"${id}"){
+      learningObjectives{
+        order
       }
     }
+  }
+  `;
+
+    const res = await callLocalGraphqlApi(query);
+    const learningObjectives = get(res, 'data.topic.learningObjectives');
+    if (learningObjectives) {
+      for (const learningObjective of learningObjectives) {
+        if (learningObjective.order === order) {
+          throw new OrderAlreadyExistsError();
+        }
+      }
+    }
+    return true;
   }
   return true;
 };
