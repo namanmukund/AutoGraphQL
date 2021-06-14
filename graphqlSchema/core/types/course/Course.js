62f813c7fda77e9b6d38366b068b63d3ba4a0a00
namanmukund <namanmukund@gmail.com>
2021-06-14 15:42:59 +0530

    length check modified

--- graphqlSchema/core/types/course/Course.js
+++ graphqlSchema/core/types/course/Course.js
@@ -38,7 +38,7 @@ const Course = `
     order: Int
     title: String! @unique
     category: CourseCategory!
-    description: String @uniqueOrEmpty @length(min: 6, max: 200) @trim
+    description: String @uniqueOrEmpty @length(min: 6, max: 800) @trim
     status: ContentStatus! @defaultValue(value: "unpublished")
     chapters: [Chapter] @relation(name: "CourseChapter")
     products: [Product] @relation(name: "CourseProduct")
