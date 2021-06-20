080c8727cfe403c3afd050fd74616350446d6e5a
Kritesh Patel <kriteshpk@gmail.com>
2021-06-21 04:59:37 +0530

    adding ME countries

--- graphqlSchema/core/types/collections/enum/Country.js
+++ graphqlSchema/core/types/collections/enum/Country.js
@@ -2,6 +2,17 @@ const Country = `
   enum Country {
     india
     usa
+    uk
+    canada
+    jamaica
+    australia
+    singapore
+    bangladesh
+    qatar
+    uae
+    oman
+    kuwait
+    egypt
   }`;
 
 export default Country;
