baedd1f5a61de993685117223699db9954d293ec
Sanatan Chaudhary <sanatanchaudhary@Sanatans-MacBook-Pro.local>
2021-06-22 02:31:50 +0530

    f

--- src/autoGenerate/utils/sendWhatsAppTemplateMessage.js
+++ src/autoGenerate/utils/sendWhatsAppTemplateMessage.js
@@ -8,6 +8,7 @@ const sendWhatsAppTemplateMessage = async (
   parameters,
 ) => {
   if (process.env.NODE_ENV !== 'production') return null;
+  if (!phoneNumber.startsWith('91')) return null;
   const bodyJson = {
     template_name,
     broadcast_name: broadcast_name || 'Tekie',
