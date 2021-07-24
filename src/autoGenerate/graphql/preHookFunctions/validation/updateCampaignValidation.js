f57a176ff95f70308fc7df45260977fd944a790b
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-07-24 16:35:09 +0530

    change base to master-staging

--- src/autoGenerate/graphql/preHookFunctions/validation/updateCampaignValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/updateCampaignValidation.js
@@ -8,7 +8,43 @@ const getCampaignData = async (id) => {
     query{
       campaign(id:"${id}"){
         id
+        school{
+          id
+        }
         batchCreationStatus
+        timeTableRules{
+          bookingDate
+          slot0
+          slot1
+          slot2
+          slot3
+          slot4
+          slot5
+          slot6
+          slot7
+          slot8
+          slot9
+          slot10
+          slot11
+          slot12
+          slot13
+          slot14
+          slot15
+          slot16
+          slot17
+          slot18
+          slot19
+          slot20
+          slot21
+          slot22
+          slot23
+          allottedMentor{
+            id
+          }
+          mentorSession{
+            id
+          }
+        }
       }
     }
   `;
@@ -38,6 +74,10 @@ const updateCampaignValidation = async (params, mutationName, context) => {
       await validateTimeTableRule(timeTableRulesArray[i]);
     }
   }
+  const preParsedTimetableRules = campaignDoc && campaignDoc.timeTableRules;
+  // parse the object to remove the prototype and hence compare with new timeTableRules input
+  context.prevTimeTableRules = JSON.parse(JSON.stringify(preParsedTimetableRules));
+  context.schoolId = campaignDoc && campaignDoc.school && campaignDoc.school.id;
 };
 
 export default updateCampaignValidation;
--- utils/createBatchesBasedOnBatchRules.js
+++ utils/createBatchesBasedOnBatchRules.js
@@ -1,4 +1,4 @@
-import { get } from 'lodash';
+import { get, isEqual } from 'lodash';
 import { NoSectionExists } from '../constants/errors';
 import {
   getSectionExists,
@@ -8,6 +8,7 @@ import {
   createBatchGroupByGrade,
   createBatchGroupBySection,
 } from '../src/autoGenerate/graphql/postHookFunctions/utils/updateCampaignHelperMethods';
+import extractSlotsFromInput from './extractSlotsFromInput';
 
 const createB2BBatchesBasedOnBatchRules = async (campaignId, courseId, batchRules, classesConnectIds, campaignSchoolId) => {
   if (batchRules && batchRules.batchCreationBasis && classesConnectIds && classesConnectIds.length > 0) {
@@ -34,6 +35,26 @@ const createB2B2CEventBatchesBasedOnBatchRules = async (campaignId, courseId, ba
   if (batchRules && timeTableRules && schoolId) {
     const timeTableRulesArray = get(timeTableRules, 'replace', []);
     createBatchForB2B2C(timeTableRulesArray, campaignId, courseId, schoolId, classesConnectIds, context);
+  } else if (context.prevTimeTableRules && timeTableRules) {
+    const prevTimeTableRulesArray = get(context, 'prevTimeTableRules');
+    const timeTableRulesArray = get(timeTableRules, 'replace', []);
+
+    // get diff of previous and current timetable rules
+    /* eslint-disable arrow-body-style */
+    const diff = timeTableRulesArray.filter((obj) => {
+      return !prevTimeTableRulesArray.some((obj2) => {
+        const { filteredSlots: f1 } = extractSlotsFromInput(obj);
+        const { filteredSlots: f2 } = extractSlotsFromInput(obj2);
+        return (obj.bookingDate === obj2.bookingDate
+          && isEqual(f1, f2)
+          && obj.allottedMentorConnectId === obj2.allottedMentorConnectId);
+      });
+    });
+
+    // if there exists any batches to be made
+    if (diff.length > 0) {
+      createBatchForB2B2C(diff, campaignId, courseId, context.schoolId, classesConnectIds, context);
+    }
   }
 };
 
