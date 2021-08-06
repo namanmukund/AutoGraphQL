dbb2afc76d2a1ece8560419a847c573a7994d339
sanatankc <sanatan.iit@gmail.com>
2021-08-06 14:41:42 +0530

    leadsquared

--- src/autoGenerate/graphql/postHookFunctions/leadsquared/updateMentorRescheduleLeadsquared.js
+++ src/autoGenerate/graphql/postHookFunctions/leadsquared/updateMentorRescheduleLeadsquared.js
@@ -1,5 +1,4 @@
 import { get } from 'lodash';
-import moment from 'moment';
 import updateLeadsquared from '../../../../../services/leadsquared/updateLeadSquared';
 import callLocalGraphqlApi from '../../../../api/callLocalGraphqlApi';
 
@@ -25,70 +24,55 @@ const updateMentorRescheduleLeadsquared = async (userInfo, input, params) => {
   const phoneNumber = get(userInfo, 'data.user.studentProfile.parents[0].user.phone.number');
   const mentorMenteeSession = await callLocalGraphqlApi(mentorMenteeSessionQuery(get(params, 'id')));
   const data = get(mentorMenteeSession, 'data.mentorMenteeSession');
-  const reasons = [
-    {
-      tag: get(data, 'internetIssue', false),
-      text: 'Slow/Unstable Internet',
-    },
-    {
-      tag: get(data, 'zoomIssue', false),
-      text: 'Zoom Not Installed',
-    },
-    {
-      tag: get(data, 'laptopIssue', false),
-      text: 'No Laptop - Joined Over The Phone',
-    },
-    {
-      tag: get(data, 'chromeIssue', false),
-      text: 'Google Chrome Not Installed',
-    },
-    {
-      tag: get(data, 'powerCut', false),
-      text: 'Power Cut',
-    },
-    {
-      tag: get(data, 'notResponseAndDidNotTurnUp', false),
-      text: 'No Response And Didn\'t Turn Up',
-    },
-    {
-      tag: get(data, 'turnedUpButLeftAbruptly', false),
-      text: 'Turned Up But Left Abruptly',
-    },
-    {
-      tag: get(data, 'leadNotVerifiedProperly', false),
-      text: 'Lead Is Not Verified Properly',
-    },
-    {
-      tag: get(data, 'otherReasonForReschedule', false),
-      text: 'Other Reasons',
-    },
-  ];
+
   const leadSquaredInput = {
     Phone: phoneNumber,
-    mx_Mentor_Rescheduled: get(data, 'hasRescheduled') ? 'Yes' : 'No',
-    mx_Mentor_Rescheduled_Reason: reasons.filter((reason) => reason.tag).map((reason) => reason.text).join(' , '),
-    mx_mentor_session_comment: get(data, 'sessionCommentByMentor', ''),
   };
-  if (get(params, 'input.hasRescheduled')) {
-    leadSquaredInput.mx_Lead_Status = 'Reschedule';
-    leadSquaredInput.ProspectStage = 'Reschedule';
+
+  let leadStatus = '';
+
+  if (get(params, 'input.sessionStatus') === 'completed') {
+    leadSquaredInput.mx_Student_turn_up = 'Yes';
+    leadStatus = 'Demo Completed';
   }
-  if (get(params, 'input.rescheduledDate')) {
-    leadSquaredInput.mx_Mentor_Rescheduled_Date_Time = moment(get(data, 'rescheduledDate')).utc().format('YYYY-MM-DD HH:mm:ss');
+
+  if (get(params, 'input.didNotTurnUpInSession')) {
+    leadSquaredInput.mx_Student_turn_up = 'No';
+    leadStatus = 'No turnup';
+  }
+
+  if (get(params, 'input.didNotPickTheCall')) {
+    leadSquaredInput.mx_Mentor_Call_Picked = 'No';
   }
   if (get(params, 'input.hasRescheduled')) {
-    updateLeadsquared(leadSquaredInput, false, {
-      ActivityEvent: 103,
-      Fields: [
-        {
-          SchemaName: 'Status',
-          Value: 'Reschedule',
-        },
-      ],
-    });
+    leadSquaredInput.mx_Interested_to_reschedule = 'Yes';
   } else {
-    updateLeadsquared(leadSquaredInput);
+    leadSquaredInput.mx_Interested_to_reschedule = 'Yes';
   }
+
+  // leadSquaredInput.mx_Mentor_Rescheduled_Reason = ['Internet issue', 'laptop not available'];
+
+  if (!get(params, 'input.didNotTurnUpInSession', true) && !get(params, 'input.hasRescheduled')) {
+    leadStatus = 'Reachout to Reschedule';
+  }
+
+  if (get(params, 'input.hasRescheduled') && !get(params, 'input.rescheduledDateProvided')) {
+    leadStatus = 'Interested to Reschedule';
+  }
+
+  if (get(params, 'input.hasRescheduled') && !get(params, 'input.rescheduledDateProvided')) {
+    leadStatus = 'Reachout to Reschedule';
+  }
+
+  updateLeadsquared(leadSquaredInput, false, {
+    ActivityEvent: 103,
+    Fields: [
+      {
+        SchemaName: 'Status',
+        Value: leadStatus,
+      },
+    ],
+  });
 };
 
 export default updateMentorRescheduleLeadsquared;
