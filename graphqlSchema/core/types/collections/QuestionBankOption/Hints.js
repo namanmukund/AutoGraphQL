07eba4c1b308d79dbf8c467409c0c33fd218b80a
sanatankc <sanatan.iit@gmail.com>
2021-08-17 15:07:10 +0530

    hints -> hint

--- graphqlSchema/core/types/collections/QuestionBankOption/Hints.js
+++
@@ -1,7 +0,0 @@
-const Hints = `
-  type Hints {
-   hintPretext: String
-   hint: String
- }`;
-
-export default Hints;
--- graphqlSchema/core/types/collections/QuestionBankOption/index.js
+++ graphqlSchema/core/types/collections/QuestionBankOption/index.js
@@ -2,12 +2,12 @@ import McqOption from './McqOption';
 import FibBlocksOption from './FibBlocksOption';
 import FibInputOption from './FibInputOption';
 import ArrangeOption from './ArrangeOption';
-import Hints from './Hints';
+import Hint from './Hint';
 
 export default [
   McqOption,
   FibBlocksOption,
   FibInputOption,
   ArrangeOption,
-  Hints,
+  Hint,
 ];
