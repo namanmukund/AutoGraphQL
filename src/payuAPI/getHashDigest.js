dce3a0d505e55ea3a3ddcf1091b88fddbe0b0984
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-09-03 09:51:19 +0530

    Remove console

--- src/payuAPI/getHashDigest.js
+++ src/payuAPI/getHashDigest.js
@@ -3,7 +3,6 @@ const crypto = require('crypto');
 const mySecret = process.env.PAYU_API_SECRET;
 
 const getHashDigest = (object) => {
-  console.log('object', object);
   const shasum = crypto.createHmac('sha256', mySecret);
   shasum.update(JSON.stringify(object));
   return shasum.digest('hex');
--- src/payuAPI/payUController.js
+++ src/payuAPI/payUController.js
@@ -10,7 +10,6 @@ import fetchUsers from './query/fetchUsers';
 
 const users = async (req, res) => {
   const digest = getHashDigest(req.query);
-  console.log('digest', digest);
   log(`digest ${digest}`);
   let foundError = false;
   let userExists = false;
