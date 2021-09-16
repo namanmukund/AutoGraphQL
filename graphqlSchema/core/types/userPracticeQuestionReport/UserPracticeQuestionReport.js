745ff4655d32155bb6eff00849652c045aef824d
sanatankc <sanatan.iit@gmail.com>
2021-09-16 09:58:28 +0530

    user courses

--- graphqlSchema/core/types/userPracticeQuestionReport/UserPracticeQuestionReport.js
+++ graphqlSchema/core/types/userPracticeQuestionReport/UserPracticeQuestionReport.js
@@ -5,6 +5,17 @@ const UserPracticeQuestionReportNextComponentType = `
    topic: Topic @relation(name: "UserPracticeQuestionReportNextComponentTypeTopic", direction: "OneWay")
  }`;
 
+const PracticeQuestionDetailedReport = `
+  type PracticeQuestionDetailedReport {
+   question: QuestionBank @relation(name: "QuestionBankMessage", direction: "OneWay")
+   isCorrect: Boolean
+   firstTry: Boolean
+   secondTry: Boolean
+   thirdOrMoreTry: Boolean
+   isAnswerUsed: Boolean
+   isHintUsed: Boolean
+ }`;
+
 const UserPracticeQuestionReport = `
   type UserPracticeQuestionReport @model {
     learningObjective: LearningObjective @relation(name: "UserPQReport", direction: "OneWay")
@@ -16,7 +27,8 @@ const UserPracticeQuestionReport = `
     answerUsedCount: Int
     nextComponent: UserPracticeQuestionReportNextComponentType @readOnly
     course: Course @relation(name: "UserPracticeQuestionReportCourse", direction: "OneWay")
+    detailedReport: [PracticeQuestionDetailedReport]
   }
 `;
 
-export default [UserPracticeQuestionReport, UserPracticeQuestionReportNextComponentType];
+export default [UserPracticeQuestionReport, UserPracticeQuestionReportNextComponentType, PracticeQuestionDetailedReport];
