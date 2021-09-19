b67c7e83ed777bfa0e9664ecc9b9f9e55e99bf15
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-09-19 13:42:48 +0530

    arrange script

--- src/autoGenerate/graphql/resolvers/query/methods/temporaryScript.js
+++ src/autoGenerate/graphql/resolvers/query/methods/temporaryScript.js
@@ -1,5 +1,4 @@
 import validateAuthentication from '../../../../../../utils/validateAuthentication';
-import updateQuestionsWithCorrectPositionIndex from '../scriptMethods/updateQuestionsWithCorrectPositionIndex';
 // import updateCourseInSalesOperationScript from '../scriptMethods/updateCourseinSalesOperationScript';
 // import updateQuestionsWithCorrectPositions from '../scriptMethods/updateQuestionsWithCorrectPositions';
 // import updateQuestionsWithHints from '../scriptMethods/updateQuestionsWithHints';
@@ -52,7 +51,7 @@ const temporaryScript = (async (root, params, context) => {
   // await migrateBatchAttendanceToEnum();
   // await updateQuestionsWithCorrectPositions();
   // await updateQuestionsWithHints();
-  await updateQuestionsWithCorrectPositionIndex();
+  // await updateQuestionsWithCorrectPositionIndex();
   return {
     result: true,
   };
