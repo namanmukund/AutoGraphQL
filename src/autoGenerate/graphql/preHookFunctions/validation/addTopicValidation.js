74a8f34db46deff01973b9e25c03955c920f4311
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-09-20 14:53:31 +0530

    topic preHook fix

--- src/autoGenerate/graphql/preHookFunctions/validation/addTopicValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/addTopicValidation.js
@@ -1,7 +1,7 @@
 import { get } from 'lodash';
 import {
   TopicWithSimilarOrderAlreadyExist,
-  TopicWithSimilarTitleAlreadyExist, MissingMandatoryInputInRequestError,
+  TopicWithSimilarTitleAlreadyExist,
 } from '../../../../../constants/errors';
 import callLocalGraphqlApi from '../../../../api/callLocalGraphqlApi';
 
@@ -21,28 +21,23 @@ export const getTopicsData = async (courseIds, title, order, topicFilter) => {
 
 const addTopicValidation = async (params) => {
   const { coursesConnectIds = [], input: { title, order } } = params;
-  if (coursesConnectIds.length === 0) {
-    throw new MissingMandatoryInputInRequestError({
-      data: {
-        message: 'Course Ids is missing in input',
-      },
-    });
-  }
-  if (title || order) {
-    let courseIds = '';
-    coursesConnectIds.forEach((courseId) => { courseIds += `"${courseId}"`; });
-    // check if the topic with similar title exist
-    if (title) {
-      const topicsDataForTitle = await getTopicsData(courseIds, title);
-      if (topicsDataForTitle && topicsDataForTitle.length > 0) {
-        throw new TopicWithSimilarTitleAlreadyExist();
+  if (coursesConnectIds.length > 0) {
+    if (title || order) {
+      let courseIds = '';
+      coursesConnectIds.forEach((courseId) => { courseIds += `"${courseId}"`; });
+      // check if the topic with similar title exist
+      if (title) {
+        const topicsDataForTitle = await getTopicsData(courseIds, title);
+        if (topicsDataForTitle && topicsDataForTitle.length > 0) {
+          throw new TopicWithSimilarTitleAlreadyExist();
+        }
       }
-    }
-    // check if the topic with similar order exist
-    if (order) {
-      const topicsDataForTitle = await getTopicsData(courseIds, null, order);
-      if (topicsDataForTitle && topicsDataForTitle.length > 0) {
-        throw new TopicWithSimilarOrderAlreadyExist();
+      // check if the topic with similar order exist
+      if (order) {
+        const topicsDataForTitle = await getTopicsData(courseIds, null, order);
+        if (topicsDataForTitle && topicsDataForTitle.length > 0) {
+          throw new TopicWithSimilarOrderAlreadyExist();
+        }
       }
     }
   }
