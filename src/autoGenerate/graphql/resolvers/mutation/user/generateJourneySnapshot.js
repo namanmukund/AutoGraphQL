04e7302bb5c568c708acdb8432b6cbba0fb8caae
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-09-30 10:27:40 +0530

    Fetch user approved codes

--- src/autoGenerate/graphql/resolvers/mutation/user/generateJourneySnapshot.js
+++ src/autoGenerate/graphql/resolvers/mutation/user/generateJourneySnapshot.js
@@ -16,6 +16,19 @@ const fetchUserCourseCompletion = (uccId) => `
   }
   `;
 
+// query to fetch user approved codes and reaction counts to determine shorter/longer template to choose
+const fetchUserApprovedCodes = (userId) => `
+  query{
+    userApprovedCodes(filter: {
+      user_some:{id: "${userId}"}
+    }){
+      id
+      totalReactionCount
+    }
+  }
+  `;
+
+
 const generateJourneySnapshotMutationResolver = async (
   root,
   params,
@@ -28,6 +41,7 @@ const generateJourneySnapshotMutationResolver = async (
   validateAuthentication(context);
   const { input } = params;
   const userCourseCompletionId = get(input, 'userCourseCompletionId', '');
+  const userId = get(input, 'userId', '');
   console.log('userCourseCompletionId', userCourseCompletionId);
   const fetchUserCourseCompletionRes = await callLocalGraphqlApi(fetchUserCourseCompletion(userCourseCompletionId));
   // TODO : check whether there exists already a saved image of journey snapshot in user course completion
@@ -37,8 +51,16 @@ const generateJourneySnapshotMutationResolver = async (
     return get(journeySnapshotFile, 'uri', '');
   }
   // TODO : if no then proceed..
-
   // TODO : to generate the image, first use 'pdf-lib' to insert the dynamic elements into the pdf and then save it as an image.
+  // TODO : check if the user has more than 0 codes published
+  const fetchUserApprovedCodesRes = await callLocalGraphqlApi(fetchUserApprovedCodes(userId));
+  let useLongerTemplate = false;
+  const userApprovedCodes = get(fetchUserApprovedCodesRes, 'data.userApprovedCodes', []);
+  if (userApprovedCodes && userApprovedCodes.length > 0) {
+    // TODO : user the longer template else use the smaller template
+    useLongerTemplate = true;
+  }
+
   return '/sample/uri';
 };
 
