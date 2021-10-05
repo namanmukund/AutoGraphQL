904064c0f489c8f018bd44e7c2780d6b96df3c9c
Smith <smith.gajjar@gmail.com>
2021-10-05 22:13:33 +0530

    fix

--- src/autoGenerate/graphql/resolvers/mutation/userData/userFirstAndLatestQuizReport.js
+++ src/autoGenerate/graphql/resolvers/mutation/userData/userFirstAndLatestQuizReport.js
@@ -294,11 +294,18 @@ const userFirstAndLatestQuizReportMutationResolver = async (
     currentRunningTopic = currentTopicComponentInfo && currentTopicComponentInfo.currentTopic;
     currentRunningTopicComponentType = currentTopicComponentInfo && currentTopicComponentInfo.currentTopicComponentType;
   }
-  if (topicInfo.order > currentRunningTopic.order) {
-    throw new ComponentLockedError();
-  } else if (topicInfo.order === currentRunningTopic.order) {
-    if (!batchCurrentComponentInfo && currentRunningTopicComponentType !== 'quiz') {
+  if (!courseId || courseId === OLD_COURSE_ID) {
+    /* eslint no-lonely-if:0 */
+    if (topicInfo.order >= currentRunningTopic.order) {
       throw new ComponentLockedError();
+    }
+  } else {
+    if (topicInfo.order > currentRunningTopic.order) {
+      throw new ComponentLockedError();
+    } else if (topicInfo.order === currentRunningTopic.order) {
+      if (!batchCurrentComponentInfo && currentRunningTopicComponentType !== 'quiz') {
+        throw new ComponentLockedError();
+      }
     }
   }
   // If not equal then check if not quiz and throw eror ( allow for batch )
