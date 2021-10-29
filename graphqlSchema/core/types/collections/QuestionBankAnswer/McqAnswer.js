c026612b4982dfa66649ace85a44a4cb880b1bcf
Smith Gajjar <smith.gajjar@gmail.com>
2021-10-29 15:15:37 +0530

    fix

--- graphqlSchema/core/types/collections/QuestionBankAnswer/McqAnswer.js
+++ graphqlSchema/core/types/collections/QuestionBankAnswer/McqAnswer.js
@@ -2,6 +2,9 @@ const McqAnswer = `
   type McqAnswer {
    statement: String
    isSelected: Boolean @defaultValue(value: "false")
+   initialXML: String
+   blocksJSON: String
+   questionBankImage: QuestionBankImage @relation(name: "McqQuestionBankImage", direction: "OneWay")
  }`;
 
 export default McqAnswer;
