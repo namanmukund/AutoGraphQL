1421d7e57cda0ede48f1dbef9fdd52974a0b570f
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-11-02 23:31:37 +0530

    Fix

--- src/autoGenerate/graphql/preHookFunctions/validation/addAdhocSessionValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/addAdhocSessionValidation.js
@@ -137,6 +137,9 @@ const addAdhocSessionValidation = async (params, mutationOrQueryName, context) =
   }
 
   // check is for given previous topic and type, if adhoc session exists from before throw err
+  // console.log('batchId', batchId)
+  // console.log('previousTopicConnectId', previousTopicConnectId)
+  // console.log('type', type)
   const getAdhocSessionRes = await callLocalGraphqlApi(getAdhocSession(batchId, previousTopicConnectId, type));
   const adhocSessions = get(getAdhocSessionRes, 'data.adhocSessions');
   // console.log('adhocSessions', adhocSessions);
