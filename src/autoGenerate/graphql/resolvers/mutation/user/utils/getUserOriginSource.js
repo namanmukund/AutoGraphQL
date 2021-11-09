38a6bbf12eccaa151c9471f5e43ae91149d62a18
sanatankc <sanatan.iit@gmail.com>
2021-11-09 14:15:07 +0530

    only school id

--- src/autoGenerate/graphql/resolvers/mutation/user/utils/getUserOriginSource.js
+++ src/autoGenerate/graphql/resolvers/mutation/user/utils/getUserOriginSource.js
@@ -17,7 +17,7 @@ const getUserOriginSource = (utmSource, schoolName = '', schoolId = '') => {
   if (utmSource && utmSource.toLowerCase().includes('google')) {
     source = google;
   }
-  if (schoolName) {
+  if (schoolId) {
     source = school;
   }
   if (schoolName && utmSource && utmSource.toLowerCase().includes('radiostreet')) {
