9345ef4b4d4af3af58c0ee76903b3894194bd47b
sanatankc <sanatan.iit@gmail.com>
2021-11-10 14:43:11 +0530

    event date time

--- src/autoGenerate/graphql/resolvers/mutation/user/parentChildSignUp.js
+++ src/autoGenerate/graphql/resolvers/mutation/user/parentChildSignUp.js
@@ -458,7 +458,7 @@ If coming from campaign and the type os b2b allocate the user to the right batch
         Phone: get(parentPhone, 'number'),
         mx_Event_Date: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '13 November' : '14 November',
         mx_Event_Time: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '03:00 pm' : '11:00 am',
-        mx_Event_Date_Time: moment().utc().format('YYYY-MM-DD HH:mm:ss'),
+        mx_Event_Date_Time: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '2021-11-13 09:30:00' : '2021-11-14 05:30:00',
       }, false, {
         ActivityEvent: 208,
         Fields: [
