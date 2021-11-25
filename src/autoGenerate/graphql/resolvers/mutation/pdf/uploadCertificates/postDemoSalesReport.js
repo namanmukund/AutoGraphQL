92bb9547093584a552cf455387ef31e48fc0e03d
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-11-25 14:34:01 +0530

    wip

--- src/autoGenerate/graphql/resolvers/mutation/pdf/uploadCertificates/postDemoSalesReport.js
+++ src/autoGenerate/graphql/resolvers/mutation/pdf/uploadCertificates/postDemoSalesReport.js
@@ -20,6 +20,8 @@ const round5 = (x) => Math.round(x / 5) * 5;
 
 const getDialUrl = (score) => `${process.env.FILE_BASE_URL}/python/course/iqaScores/score${round5(score)}.png`;
 
+const getMentorDialUrl = (score) => `${process.env.FILE_BASE_URL}/python/course/ratingsDial/rating${round5(score)}.png`;
+
 const getMentorRatingStars = (rating) => `${process.env.FILE_BASE_URL}/python/course/mentorRatings/${rating}star.png`;
 
 const iqaReportQuery = (id) => `{
@@ -66,7 +68,7 @@ const getPostDemoSalesReportUrl = async (userId) => {
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
     const secondPage = pages[1];
-    const thirdPage = pages[3];
+    const thirdPage = pages[2];
     const { width } = firstPage.getSize();
     // // Embed the Helvetica font
     const GilroyExtraBoldfontBytes = await fetch(GILROY_EXTRA_BOLD_FONT_URL).then((res) => res.buffer());
@@ -74,20 +76,166 @@ const getPostDemoSalesReportUrl = async (userId) => {
     const NunitoBoldfontBytes = await fetch(NUNITO_BOLD_FONT_URL).then((res) => res.buffer());
     const NunitoBoldFont = await pdfDoc.embedFont(NunitoBoldfontBytes);
 
-    firstPage.drawText(`${'Parent Name'}`, {
+    const dialImageBytes = await fetch(getDialUrl(get(iqaReports, '[0].iqaScoree', 70))).then((res) => res.buffer());
+
+    const dialImage = await pdfDoc.embedPng(dialImageBytes);
+    const dialDims = dialImage.scale(1);
+
+    const mentorRatingStarBytes = await fetch(getMentorRatingStars(5)).then((res) => res.buffer());
+    const mentorRatingStarImage = await pdfDoc.embedPng(mentorRatingStarBytes);
+    const mentorRatingStarDim = mentorRatingStarImage.scale(1);
+
+    const mentorRatingDialBytes = await fetch(getMentorDialUrl(5)).then((res) => res.buffer());
+    const mentorRatingDialImage = await pdfDoc.embedPng(mentorRatingDialBytes);
+    const mentorRatingDialDim = mentorRatingDialImage.scale(1);
+
+    /*
+      FIRST PAGE
+    */
+    firstPage.drawText(`${'Parent Name'},`, {
       x: 100,
-      y: 675,
+      y: 678,
       size: 15,
       font: NunitoBoldFont,
       color: rgb(0, 0.29, 0.678),
     });
 
-    firstPage.drawText(`${'Student Name'}`, {
-      x: 150,
-      y: 625,
+    firstPage.drawText(`${'Student Name'}.`, {
+      x: 166,
+      y: 623,
       size: 15,
       font: NunitoBoldFont,
       color: rgb(0, 0.29, 0.678),
+    });
+
+    /*
+      SECOND PAGE
+    */
+    secondPage.drawText(`${get(iqaReports, '[0].iqaScore', 70)}`, {
+      x: 170,
+      y: 640,
+      size: 50,
+      font: GilroyExtraBoldFont,
+      color: rgb(0, 0.29, 0.678),
+    });
+
+    secondPage.drawText(`/${get(iqaReports, '[0].maximumScore', 100)}`, {
+      x: 170,
+      y: 610,
+      size: 20,
+      font: GilroyExtraBoldFont,
+      color: rgb(0.522, 0.518, 0.518),
+    });
+
+    secondPage.drawText(`${get(iqaReports, '[0].iqaRank', 70)}`, {
+      x: 415,
+      y: 697,
+      size: 20,
+      font: NunitoBoldFont,
+      color: rgb(0.314, 0.310, 0.310),
+    });
+
+    secondPage.drawText(`${get(iqaReports, '[0].globalRank', 70)}`, {
+      x: 415,
+      y: 609,
+      size: 20,
+      font: NunitoBoldFont,
+      color: rgb(0.314, 0.310, 0.310),
+    });
+
+    // dial image
+    secondPage.drawImage(dialImage, {
+      x: 87,
+      y: 562,
+      width: dialDims.width,
+      height: dialDims.height,
+    });
+
+    secondPage.drawText(`${get(iqaReports, '[0].user.name', '').split(' ')[0].toUpperCase()}`, {
+      x: 115,
+      y: 490,
+      size: 12,
+      font: NunitoBoldFont,
+      color: rgb(0.314, 0.310, 0.310),
+    });
+
+    secondPage.drawText(`${get(iqaReports, '[0].user.name', '').split(' ')[0].toUpperCase()}`, {
+      x: 225,
+      y: 490,
+      size: 12,
+      font: NunitoBoldFont,
+      color: rgb(0.314, 0.310, 0.310),
+    });
+
+    secondPage.drawText(`${get(iqaReports, '[0].user.name', '').split(' ')[0].toUpperCase()}`, {
+      x: 315,
+      y: 490,
+      size: 12,
+      font: NunitoBoldFont,
+      color: rgb(0.314, 0.310, 0.310),
+    });
+
+    // experience in coding
+    secondPage.drawText(`${'3.5+ Years'}`, {
+      x: 317,
+      y: 360,
+      size: 14,
+      font: NunitoBoldFont,
+      color: rgb(0, 0.29, 0.678),
+    });
+
+    // most common review
+    secondPage.drawText(`${'Friendly & Dedicated'}`, {
+      x: 317,
+      y: 307,
+      size: 14,
+      font: NunitoBoldFont,
+      color: rgb(0, 0.29, 0.678),
+    });
+
+    // Languages
+    secondPage.drawText(`${'Java, Python'}`, {
+      x: 317,
+      y: 254,
+      size: 14,
+      font: NunitoBoldFont,
+      color: rgb(0, 0.29, 0.678),
+    });
+
+    // languages familiar with
+    secondPage.drawText(`${capitalize('Gokul Madhusudhan')}`, {
+      x: ((width - getStringWidth(`${capitalize('Gokul Madhusudhan')}`)) / 4) + 40,
+      y: 257,
+      size: 14,
+      font: NunitoBoldFont,
+      color: rgb(0, 0.29, 0.678),
+    });
+
+    // mentor rating stars
+    secondPage.drawImage(mentorRatingStarImage, {
+      x: 155,
+      y: 226,
+      width: mentorRatingStarDim.width,
+      height: mentorRatingStarDim.height,
+    });
+
+    // mentor circle
+    secondPage.drawCircle({
+      x: 200,
+      y: 320,
+      size: 40,
+      color: rgb(0.969, 0.729, 0.290),
+      opacity: 1,
+    });
+    /*
+      THIRD PAGE
+    */
+    // mentor rating dial
+    thirdPage.drawImage(mentorRatingDialImage, {
+      x: 150,
+      y: 700,
+      width: mentorRatingDialDim.width,
+      height: mentorRatingDialDim.height,
     });
   } else {
     url = `${process.env.FILE_BASE_URL}/python/course/postDemoPreTest.pdf`;
