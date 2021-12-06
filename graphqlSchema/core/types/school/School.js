af59801a42f3934d16aaf7b528c5b877a800fba0
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-12-06 17:45:31 +0530

    added: sensei role

--- graphqlSchema/core/types/school/School.js
+++ graphqlSchema/core/types/school/School.js
@@ -19,7 +19,7 @@ const School = `
     schoolPicture: File @relation(name: "SchoolPicture", direction: "OneWay")
     hubspotId: String
     schoolCampaignCode: String @unique @trim
-    commsEnabled: Boolean @defaultValue(value: "false")
+    isCommsEnabled: Boolean @defaultValue(value: "false")
   }
 `;
 export default [School];
