5f3498387c2646d21feee653f7d569e6a48dfdbd
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-12-09 23:40:58 +0530

    lead partner changes

--- graphqlSchema/core/types/leadPartner/LeadPartner.js
+++ graphqlSchema/core/types/leadPartner/LeadPartner.js
@@ -2,6 +2,7 @@ const LeadPartner = `
   type LeadPartner @model {
     utmDetails: [UtmDetail] @relation(name: "LeadPartnerUtmDetail")
     title: String!
+    agent: User @relation(name:"LeadPartnerUser", direction: "OneWay")
 }`;
 
 export default [LeadPartner];
