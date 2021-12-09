29a28bcc6f53e95c307c1d50aeb05eb40a660079
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-12-09 14:45:08 +0530

    senseiTeam schema changes

--- graphqlSchema/core/types/senseiProfile/SenseiProfile.js
+++ graphqlSchema/core/types/senseiProfile/SenseiProfile.js
@@ -2,7 +2,7 @@ const SenseiProfile = `
   type SenseiProfile @model {
     user: User! @relation(name: "SenseiProfileUser")
     mentors: [MentorProfile] @relation(name: "SenseiProfileMentorProfile")
-    SenseiMentorTeam: [SenseiMentorTeam] @relation(name: "SenseiProfileSenseiMentorTeam")
+    senseiMentorTeams: [SenseiMentorTeam] @relation(name: "SenseiProfileSenseiMentorTeam")
 }`;
 
 export default [SenseiProfile];
