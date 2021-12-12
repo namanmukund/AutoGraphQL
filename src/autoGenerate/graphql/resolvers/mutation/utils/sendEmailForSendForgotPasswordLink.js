a99e55f32dbcaff78ea0ec3ffa55809c4c1a446b
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-12-12 23:43:43 +0530

    template added

--- src/autoGenerate/graphql/resolvers/mutation/utils/sendEmailForSendForgotPasswordLink.js
+++ src/autoGenerate/graphql/resolvers/mutation/utils/sendEmailForSendForgotPasswordLink.js
@@ -1,9 +1,9 @@
 import { sendForgotPasswordLinkToUser } from '../../../../../email/messages';
 
 const sendEmailForSendForgotPasswordLink = (fetchedUser, authentication, forgotPassLink) => {
-  const { email } = fetchedUser;
+  const { email, name } = fetchedUser;
   const appName = authentication.app.name;
-  sendForgotPasswordLinkToUser(email, forgotPassLink, appName);
+  sendForgotPasswordLinkToUser(email, forgotPassLink, appName, name);
   return null;
 };
 
