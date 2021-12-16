5efa6414e091bc008c4e99fde96dcf133f10bd27
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-12-17 01:19:48 +0530

    lead country fieldS

--- graphqlSchema/core/types/leadPartner/LeadPartner.js
+++ graphqlSchema/core/types/leadPartner/LeadPartner.js
@@ -5,11 +5,19 @@ const LeadPartner = `
     title: String!
 }`;
 
+const Timezones = `
+ type Timezones {
+   value: String
+ }
+`;
+
 const LeadPartnerAgent = `
   type LeadPartnerAgent @model {
-    utmDetails: [UtmDetail] @relation(name: "LeadPartnerAgentUtmDetail") 
+    utmDetails: [UtmDetail] @relation(name: "LeadPartnerAgentUtmDetail")
     leadPartner: LeadPartner @relation(name:"LeadPartnerAgentUser")
+    countries: [Countries]
     agent: User @relation(name:"LeadPartnerAgentUser", direction: "OneWay")
+    timezones: [Timezones]
 }`;
 
-export default [LeadPartner, LeadPartnerAgent];
+export default [LeadPartner, LeadPartnerAgent, Timezones];
