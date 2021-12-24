34148ae0de897d935733185070504966f1010b97
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-12-24 14:58:44 +0530

    savedPassword fix

--- src/autoGenerate/graphql/preHookFunctions/validation/updateUserValidation.js
+++ src/autoGenerate/graphql/preHookFunctions/validation/updateUserValidation.js
@@ -124,7 +124,7 @@ const updateUserValidation = async (params, context, mutationOrQueryName) => {
     if (!allowedRoles.includes(currentUserRole)) {
       throw new InsufficientPermissionError();
     }
-    const passwordObj = getUserPasswordObject(password, true);
+    const passwordObj = getUserPasswordObject(password, false);
     Object.assign(userObj, passwordObj);
   }
 
