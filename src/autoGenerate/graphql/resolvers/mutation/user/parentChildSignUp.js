340d20667c687305707ebf464f3f32eed645ca39
sanatankc <sanatan.iit@gmail.com>
2021-12-28 11:17:10 +0530

    changes

--- src/autoGenerate/graphql/resolvers/mutation/user/parentChildSignUp.js
+++ src/autoGenerate/graphql/resolvers/mutation/user/parentChildSignUp.js
@@ -459,9 +459,9 @@ If coming from campaign and the type os b2b allocate the user to the right batch
     setTimeout(() => {
       updateLeadSquared({
         Phone: get(parentPhone, 'number'),
-        mx_Event_Date: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '26 December' : '26 December',
+        mx_Event_Date: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '26 December' : '2 January',
         mx_Event_Time: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '03:00 pm' : '11:00 am',
-        mx_Event_Date_Time: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '2021-12-26 09:30:00' : '2021-12-26 05:30:00',
+        mx_Event_Date_Time: utmSource.includes('SpySquadCamp') || utmSource.includes('communityevent') ? '2021-12-26 09:30:00' : '2022-01-02 05:30:00',
       }, false, {
         ActivityEvent: 208,
         Fields: [
