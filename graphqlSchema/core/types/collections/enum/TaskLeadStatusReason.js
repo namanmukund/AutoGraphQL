c6e59698b85928fb53a0fcf0bb5a53d0acc07b85
Gokul Madhusudhan <gokul99.gm@gmail.com>
2022-01-07 10:22:58 +0530

    new fields

---
+++ graphqlSchema/core/types/collections/enum/TaskLeadStatusReason.js
@@ -0,0 +1,15 @@
+const TaskLeadStatusReason = `
+  enum TaskLeadStatusReason {
+    willTakeDemoReason
+    notParent
+    lookingForFreeCourse
+    dontRememberRegistering
+    choseCompetitor
+    busy
+    notReachable
+    switchedOff
+    callLater
+    ghosted
+  }`;
+
+export default TaskLeadStatusReason;
