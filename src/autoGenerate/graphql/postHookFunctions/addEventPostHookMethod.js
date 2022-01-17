ac9d07fcd7f77f3e5318a28611c6f24dd933c1e1
keshav304 <keshavjhaa2678@gmail.com>
2022-01-17 15:43:59 +0530

    published event comms

--- src/autoGenerate/graphql/postHookFunctions/addEventPostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/addEventPostHookMethod.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable no-restricted-syntax */
 import { get } from 'lodash';
-import moment from 'moment';
 import {
   addUpdateEventSessionsForEvent,
 } from './utils/updateEventPostHookQueries';
-import addToSchedule from '../../../../utils/scheduleJobs/addToSchedule';
 
 /*
   Post hook of update event
@@ -14,32 +12,8 @@ import addToSchedule from '../../../../utils/scheduleJobs/addToSchedule';
 const addEventPostHookMethod = async (input, params, mutationName, context) => {
   const { id: eventId } = input;
   const timeTableRule = get(params, 'input.eventTimeTableRule', null);
-  const eventCommsRules = get(params, 'input.eventCommsRule', null);
   if (timeTableRule) {
     addUpdateEventSessionsForEvent(eventId, timeTableRule);
-  }
-  for (const eventCommsRule of eventCommsRules) {
-    const dateCondition = get(eventCommsRule, 'condition', null);
-    switch (dateCondition) {
-      case 'before':
-        const startDate = get(timeTableRule, 'startDate', null);
-        const scheduledTime = moment(startDate).subtract(get(eventCommsRule, 'value', 0), get(eventCommsRule, 'unit', 'days'));
-        addToSchedule('eventCommsJob', scheduledTime, {
-          eventId,
-          eventCommsRule,
-        });
-        break;
-      case 'after':
-        const endDate = get(timeTableRule, 'endDate', null);
-        const scheduledTime1 = moment(endDate).add(get(eventCommsRule, 'value', 0), get(eventCommsRule, 'unit', 'days'));
-        addToSchedule('eventCommsJob', scheduledTime1, {
-          eventId,
-          eventCommsRule,
-        });
-        break;
-      default:
-        break;
-    }
   }
 };
 
--- src/autoGenerate/graphql/postHookFunctions/updateEventPostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/updateEventPostHookMethod.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-restricted-syntax */
 import { get } from 'lodash';
+import moment from 'moment';
 import {
   addUpdateEventSessionsForEvent,
 } from './utils/updateEventPostHookQueries';
 import sendCommsForUpdatedEvents from './utils/sendCommsForUpdatedEvents';
+import addToSchedule from '../../../../utils/scheduleJobs/addToSchedule';
 /*
   Post hook of update event
 */
@@ -15,6 +18,7 @@ const updateEventPostHookMethod = async (input, params, mutationName, context) =
   const shouldSendCanceledComms = get(params, 'input.shouldSendCanceledComms', false);
   const eventRescheduledReason = get(params, 'input.rescheduledReason', null);
   const eventCancellationReason = get(params, 'input.cancellationReason', null);
+  const eventCommsRules = get(params, 'input.eventCommsRule', null);
   const {
     prevTimeTableRule,
     previousEventStatus,
@@ -28,6 +32,33 @@ const updateEventPostHookMethod = async (input, params, mutationName, context) =
   }
   if (previousEventStatus === 'cancelled' && shouldSendCanceledComms) {
     sendCommsForUpdatedEvents(eventId, eventCancellationReason, 'canceled');
+  }
+  if (previousEventStatus === 'published') {
+    for (const eventCommsRule of eventCommsRules) {
+      const dateCondition = get(eventCommsRule, 'condition', null);
+      switch (dateCondition) {
+        case 'before': {
+          const startDate = get(timeTableRule, 'startDate', null);
+          const scheduledTime = moment(startDate).subtract(get(eventCommsRule, 'value', 0), get(eventCommsRule, 'unit', 'days'));
+          addToSchedule('eventCommsJob', scheduledTime, {
+            eventId,
+            eventCommsRule,
+          });
+          break;
+        }
+        case 'after': {
+          const endDate = get(timeTableRule, 'endDate', null);
+          const scheduledTime1 = moment(endDate).add(get(eventCommsRule, 'value', 0), get(eventCommsRule, 'unit', 'days'));
+          addToSchedule('eventCommsJob', scheduledTime1, {
+            eventId,
+            eventCommsRule,
+          });
+          break;
+        }
+        default:
+          break;
+      }
+    }
   }
 };
 
