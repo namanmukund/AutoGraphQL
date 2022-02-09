25c898b7e3c2ddfd940360bcf141fc91cf0fe73f
pawan-kumar-dev <pawan.kv612@gmail.com>
2022-02-09 20:35:27 +0530

    comms test

--- utils/scheduleJobs/reRunJobsFromDB.js
+++ utils/scheduleJobs/reRunJobsFromDB.js
@@ -99,174 +99,160 @@ const reRunJobsFromDB = async () => {
     } = scheduledJob;
     const deleteJob = () => callLocalGraphqlApi(deleteJobQuery(id));
     const isPast = moment().isAfter(scheduledDate);
-    if (process.env.NODE_ENV === 'staging' && (jobType !== 'eventNewRegistrationReminder' || jobType !== 'eventCommsJob')) return null;
-    const userId = get(parent, 'id');
-    switch (jobType) {
-      case 'sendB2BReminder': {
-        if (isPast) {
-          scheduleB2BSessionReminder(batchSessionId, deleteJob);
-        } else {
-          schedule.scheduleJob(new Date(scheduledDate), () => {
+    if (process.env.NODE_ENV === 'staging' && (jobType === 'eventNewRegistrationReminder' || jobType === 'eventCommsJob')) {
+      const userId = get(parent, 'id');
+      switch (jobType) {
+        case 'sendB2BReminder': {
+          if (isPast) {
             scheduleB2BSessionReminder(batchSessionId, deleteJob);
-          });
+          } else {
+            schedule.scheduleJob(new Date(scheduledDate), () => {
+              scheduleB2BSessionReminder(batchSessionId, deleteJob);
+            });
+          }
+          break;
         }
-        break;
-      }
-      case 'sendB2BHomeworkReminder': {
-        if (isPast) {
-          scheduleB2BSessionHomeworkRemainder(batchSessionId, deleteJob);
-        } else {
-          schedule.scheduleJob(new Date(scheduledDate), () => {
+        case 'sendB2BHomeworkReminder': {
+          if (isPast) {
             scheduleB2BSessionHomeworkRemainder(batchSessionId, deleteJob);
-          });
+          } else {
+            schedule.scheduleJob(new Date(scheduledDate), () => {
+              scheduleB2BSessionHomeworkRemainder(batchSessionId, deleteJob);
+            });
+          }
+          break;
         }
-        break;
-      }
-      case 'sendNextDayBookReminder': {
-        if (isPast) {
-          sendB2B2CBookReminderNextDay({ userId: get(parent, 'id'), code }, deleteJob);
-        } else {
-          schedule.scheduleJob(new Date(scheduledDate), () => {
+        case 'sendNextDayBookReminder': {
+          if (isPast) {
             sendB2B2CBookReminderNextDay({ userId: get(parent, 'id'), code }, deleteJob);
-          });
+          } else {
+            schedule.scheduleJob(new Date(scheduledDate), () => {
+              sendB2B2CBookReminderNextDay({ userId: get(parent, 'id'), code }, deleteJob);
+            });
+          }
+          break;
         }
-        break;
-      }
-      case 'sendB2CBookReminderNextDay': {
-        if (isPast) {
-          sendB2CBookReminderNextDay({ userId }, deleteJob);
-        } else {
-          schedule.scheduleJob(new Date(scheduledDate), () => {
+        case 'sendB2CBookReminderNextDay': {
+          if (isPast) {
             sendB2CBookReminderNextDay({ userId }, deleteJob);
-          });
+          } else {
+            schedule.scheduleJob(new Date(scheduledDate), () => {
+              sendB2CBookReminderNextDay({ userId }, deleteJob);
+            });
+          }
+          break;
         }
-        break;
-      }
-      case 'engagementMail': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
-        });
-        break;
-      }
-      case 'engagementMailWithMentor': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
-        });
-        break;
-      }
-      case 'bookingFinalReminder': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
-        });
-        break;
-      }
-      case 'bookingSameDayFinalReminder': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
-        });
-        break;
-      }
-      case 'sessionReminderWati': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
-        });
-        break;
-      }
-      case 'postCarnivalMail': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          extractBatchSessionAndPostCarnival({ batchSessionId }, deleteJob);
-        });
-        break;
-      }
-      case 'B2CEngagementMail': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2CSessionReminder({
-            userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'B2CEngagementMailWithMentor': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2CSessionReminder({
-            userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'B2CBookingFinalReminder': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2CSessionReminder({
-            userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'B2CBookingSameDayFinalReminder': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2CSessionReminder({
-            userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'B2CSessionReminderWati': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendB2CSessionReminder({
-            userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'mentorSessionNotificationB2C': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendMentorSessionReminder({
-            mentorMenteeSessionId, jobType,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'mentorSessionNotificationB2B2C': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendMentorSessionReminderB2B2C({
-            jobType,
-            batchSessionId,
-            courseName,
-            batchCode,
-            schoolName,
-            sessionDate,
-            sessionTime,
-            sessionLink,
-            mentorUserId,
-            mentorPhoneNumber,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'sendMentorVerifyBookingReminder': {
-        schedule.scheduleJob(new Date(scheduledDate), () => {
-          sendMentorVerifyBookingReminder({
-            taskId, mentorUserId, jobType,
-          }, deleteJob);
-        });
-        break;
-      }
-      case 'eventCommsJob': {
-        if (isPast) {
-          sendEventCommunication({
-            eventId,
-            jobType,
-            commsVariables,
-            templateName,
-            isEmailRule,
-            condition,
-            attendanceFilter,
-            value,
-            unit,
-            jobId: id,
-          }, deleteJob);
-        } else {
+        case 'engagementMail': {
           schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
+          });
+          break;
+        }
+        case 'engagementMailWithMentor': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
+          });
+          break;
+        }
+        case 'bookingFinalReminder': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
+          });
+          break;
+        }
+        case 'bookingSameDayFinalReminder': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
+          });
+          break;
+        }
+        case 'sessionReminderWati': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2B2CBookingReminder({ userId, code, jobType }, deleteJob);
+          });
+          break;
+        }
+        case 'postCarnivalMail': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            extractBatchSessionAndPostCarnival({ batchSessionId }, deleteJob);
+          });
+          break;
+        }
+        case 'B2CEngagementMail': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2CSessionReminder({
+              userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'B2CEngagementMailWithMentor': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2CSessionReminder({
+              userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'B2CBookingFinalReminder': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2CSessionReminder({
+              userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'B2CBookingSameDayFinalReminder': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2CSessionReminder({
+              userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'B2CSessionReminderWati': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendB2CSessionReminder({
+              userId: menteeId, jobType, menteeSessionId, menteeSessionUpdatedAt,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'mentorSessionNotificationB2C': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendMentorSessionReminder({
+              mentorMenteeSessionId, jobType,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'mentorSessionNotificationB2B2C': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendMentorSessionReminderB2B2C({
+              jobType,
+              batchSessionId,
+              courseName,
+              batchCode,
+              schoolName,
+              sessionDate,
+              sessionTime,
+              sessionLink,
+              mentorUserId,
+              mentorPhoneNumber,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'sendMentorVerifyBookingReminder': {
+          schedule.scheduleJob(new Date(scheduledDate), () => {
+            sendMentorVerifyBookingReminder({
+              taskId, mentorUserId, jobType,
+            }, deleteJob);
+          });
+          break;
+        }
+        case 'eventCommsJob': {
+          if (isPast) {
             sendEventCommunication({
               eventId,
               jobType,
@@ -278,23 +264,29 @@ const reRunJobsFromDB = async () => {
               value,
               unit,
               jobId: id,
+              isPast,
             }, deleteJob);
-          });
+          } else {
+            schedule.scheduleJob(new Date(scheduledDate), () => {
+              sendEventCommunication({
+                eventId,
+                jobType,
+                commsVariables,
+                templateName,
+                isEmailRule,
+                condition,
+                attendanceFilter,
+                value,
+                unit,
+                jobId: id,
+                isPast,
+              }, deleteJob);
+            });
+          }
+          break;
         }
-        break;
-      }
-      case 'eventNewRegistrationReminder': {
-        if (isPast) {
-          eventNewRegistrationReminder({
-            eventId,
-            jobType,
-            studentProfileId,
-            commsVariables,
-            templateName,
-            isEmailRule,
-          }, deleteJob);
-        } else {
-          schedule.scheduleJob(new Date(scheduledDate), () => {
+        case 'eventNewRegistrationReminder': {
+          if (isPast) {
             eventNewRegistrationReminder({
               eventId,
               jobType,
@@ -303,14 +295,24 @@ const reRunJobsFromDB = async () => {
               templateName,
               isEmailRule,
             }, deleteJob);
-          });
+          } else {
+            schedule.scheduleJob(new Date(scheduledDate), () => {
+              eventNewRegistrationReminder({
+                eventId,
+                jobType,
+                studentProfileId,
+                commsVariables,
+                templateName,
+                isEmailRule,
+              }, deleteJob);
+            });
+          }
+          break;
         }
-        break;
+        default:
+          break;
       }
-      default:
-        break;
     }
-    return true;
   });
 };
 
