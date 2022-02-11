1108ade02b4f6df2c67f72edb25b464f87aacaaf
Gokul Madhusudhan <gokul99.gm@gmail.com>
2022-02-11 12:16:14 +0530

    revert getPossibleDates logic

--- utils/getPossibleDates.js
+++ utils/getPossibleDates.js
@@ -1,30 +1,18 @@
 // method that returns the array of dates
-const getPossibleDates = (workingStartDate, workingEndDate, workingDays, eventStartDate, eventEndDate, eventDays) => {
-  // here workingDays is Set which hold the integer values of days of the week (0-6), for better lookup
+const getPossibleDates = (startDate, endDate, days) => {
+  // here days is Set which hold the integer values of days of the week (0-6), for better lookup
   const dates = [];
-  // if we don't have either working or event, return empty dates array
-  if (!workingStartDate && !eventStartDate) return dates;
-  let currentDate = workingStartDate || eventStartDate;
+  let currentDate = startDate;
   /* eslint-disable func-names */
   const addDays = function (daysToAdd) {
     const date = new Date(this.valueOf());
     date.setDate(date.getDate() + daysToAdd);
     return date;
   };
-  const endDate = workingEndDate || eventEndDate;
-  const considerEvents = eventStartDate && eventEndDate && eventDays;
+
   while (currentDate <= endDate) {
-    // console.log('workingDays', workingDays);
-    // console.log('considerEvents', considerEvents);
-    if (workingDays.has(currentDate.getDay())) {
-      if (considerEvents) {
-        const isEventDate = (currentDate <= eventEndDate && currentDate >= eventStartDate && eventDays.has(currentDate.getDay()));
-        if (!isEventDate) {
-          dates.push(currentDate);
-        }
-      } else {
-        dates.push(currentDate);
-      }
+    if (days.has(currentDate.getDay())) {
+      dates.push(currentDate);
     }
     currentDate = addDays.call(currentDate, 1);
   }
