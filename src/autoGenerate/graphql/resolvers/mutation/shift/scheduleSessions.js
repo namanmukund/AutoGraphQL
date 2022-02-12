0a4b8edfab87c29f9836521f2c17b23a80d16706
Gokul Madhusudhan <gokul99.gm@gmail.com>
2022-02-12 19:30:57 +0530

    batch sessions recurring schedule

--- src/autoGenerate/graphql/resolvers/mutation/shift/scheduleSessions.js
+++ src/autoGenerate/graphql/resolvers/mutation/shift/scheduleSessions.js
@@ -1,53 +1,30 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
 /* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-await-in-loop */
 import { get } from 'lodash';
 import moment from 'moment';
+import { log } from '../../../../../../utils/log';
 import validateAuthentication from '../../../../../../utils/validateAuthentication';
 import callLocalGraphqlApi from '../../../../../api/callLocalGraphqlApi';
 import getSelectedDays from '../../../postHookFunctions/utils/getSelectedDays';
 import extractSlotsFromInput from '../../../../../../utils/extractSlotsFromInput';
-import getSlotTimeFields from '../../../../../../graphqlSchema/core/functions/getSlotTimeFields';
-import getWeekDaysFields from '../../../../../../graphqlSchema/core/functions/getWeekDaysFields';
 import { CannotScheduleOutsideWorkingHoursError } from '../../../../../../constants/errors';
+import getPossibleDates from '../../../../../../utils/getPossibleDates';
+import getSelectedSlotsTime from '../../../preHookFunctions/validation/utils/getSelectedSlotsTime';
+import { weekDays, slotTimes } from '../../../../../../constants';
+import {
+  getTopics, getBatchSessions, createBatchSession, updateBatchSession,
+} from '../../../postHookFunctions/utils/updateBatchPostHookQueries';
+import {
+  fetchBatch,
+  fetchMentorSessions,
+  updateMentorSession,
+  addMentorSession,
+} from './queries/scheduleSessionsQueries';
 
-const slotTimeFields = getSlotTimeFields('Boolean', true);
-const weekDaysFields = getWeekDaysFields('Boolean', true);
-
-const fetchBatch = async (batchId) => {
-  const query = `
-    query {
-      batch(id: "${batchId}"){
-        id
-        timeTableRule {
-          ${slotTimeFields}
-          ${weekDaysFields}
-          startDate
-          endDate
-        }
-        school {
-          timetableSchedule {
-            type
-            startDate: Date
-            endDate: Date
-            ${slotTimeFields}
-            ${weekDaysFields}
-          }
-        }
-        timetableSchedule {
-          type
-          startDate: Date
-          endDate: Date
-          ${slotTimeFields}
-          ${weekDaysFields}
-        }
-      }
-    }
-  `;
-  const res = await callLocalGraphqlApi(query);
-  return get(res, 'data.batch', {});
-};
-
+// combines the working day and event schedules
 const getCombinedSchedules = (schoolTimetableScheduleArray, batchTimetableScheduleArray) => {
   const combinedWorkingDaySchedule = {};
   const combinedEventScheduleArray = [];
@@ -87,42 +64,135 @@ const getCombinedSchedules = (schoolTimetableScheduleArray, batchTimetableSchedu
 };
 
 // method to check if given schedule will lie outside working hour schedule
-const checkIfOutsideWorkingSchedule = (combinedWorkingDaySchedule, combinedEventScheduleArray, days, startDate, endDate, filteredSlots) => {
-  // check bounds
-  if (moment(startDate).isBefore(moment(combinedWorkingDaySchedule.startDate))
-  || moment(endDate).isAfter(moment(combinedWorkingDaySchedule.endDate))) {
-    return false;
+const checkIfOutsideWorkingSchedule = (combinedWorkingDaySchedule, combinedEventScheduleArray, timeTableRule) => {
+  // checking if days are within bounds
+  if (moment(timeTableRule.startDate).isBefore(moment(combinedWorkingDaySchedule.startDate))
+    || moment(timeTableRule.endDate).isAfter(moment(combinedWorkingDaySchedule.endDate))) {
+    return true;
   }
-  // check day and slot RE DO LOGIC
-  for (const slot in Object.keys(filteredSlots)) {
-    for (const key in Object.keys(combinedWorkingDaySchedule)) {
-      // check if same day and not same slot, or different day
-      if (key.includes('day') && combinedWorkingDaySchedule[key]
-      && ((days.has(key) && slot.includes('slot') && filteredSlots[slot] && !combinedWorkingDaySchedule[slot]) || !days.has[key])) {
-        return false;
-      }
+  // checking if weekdays or slots are outside scheduled working hours
+  for (const weekDay in weekDays) {
+    if (!combinedWorkingDaySchedule[weekDay] && timeTableRule[weekDay]) {
+      return true;
     }
   }
-  // loop over event schedule and check if events fall on input schedule
-  for (const schedule of combinedEventScheduleArray) {
-    // skip if dates don't intersect
-    if (moment(startDate).isAfter(moment(schedule.endDate)) || moment(endDate).isBefore(moment(schedule.endDate))) {
-      // eslint-disable-next-line no-continue
-      continue;
+  for (const slotTime in slotTimes) {
+    if (!combinedWorkingDaySchedule[slotTime] && timeTableRule[slotTime]) {
+      return true;
     }
-    // check day and slot RE DO LOGIC
-    for (const slot in Object.keys(filteredSlots)) {
-      for (const key in Object.keys(schedule)) {
-        // check if same day and same slot
-        if (key.includes('day') && schedule[key]
-          && ((days.has(key) && slot.includes('slot') && filteredSlots[slot] && !schedule[slot]) || !days.has[key])) {
-          return false;
+  }
+  // for events, we check return true if events schedule exactly matches (opposite logic for working day)
+  for (const combinedEventScheduleItem in combinedEventScheduleArray) {
+    for (const weekDay in weekDays) {
+      if (combinedEventScheduleItem[weekDay] && timeTableRule[weekDay]) {
+        for (const slotTime in slotTimes) {
+          if (combinedEventScheduleItem[slotTime] && timeTableRule[slotTime]) {
+            return true;
+          }
         }
       }
     }
   }
-  // if passed, return true
+  // if inside working schedule
+  return false;
+};
+
+// method to sort batchSessions
+const sortBatchSessions = (batchSessions) => {
+  const sessionsStartedOrCompleted = [];
+  const sessionsAllotted = [];
+
+  batchSessions.map((item) => {
+    /* eslint-disable array-callback-return */
+    if (item.sessionStatus === 'allotted') {
+      sessionsAllotted.push(item);
+    } else {
+      sessionsStartedOrCompleted.push(item);
+    }
+  });
+
+  return { sessionsStartedOrCompleted, sessionsAllotted };
+};
+
+// get mentor session if after updating / adding mentor session
+const getMentorSessionId = async (allottedMentorId, date, slotsInInput, courseId, sessionType) => {
+  let finalMentorSessionId = '';
+  if (allottedMentorId) {
+    const sentSlotsArray = getSelectedSlotsTime(slotsInInput);
+    // eslint-disable-next-line no-await-in-loop
+    const mentorSessionsRes = await callLocalGraphqlApi(fetchMentorSessions(date, allottedMentorId, sessionType));
+    const mentorSession = get(mentorSessionsRes, 'data.mentorSessions[0]');
+    if (mentorSession && mentorSession.id) {
+      const { id: mentorSessionId, ...slotsInMentorSession } = mentorSession;
+      finalMentorSessionId = mentorSessionId;
+      const slotsInMentorSessionArray = getSelectedSlotsTime(slotsInMentorSession);
+      if (sentSlotsArray && sentSlotsArray.length && slotsInMentorSessionArray && slotsInMentorSessionArray.length && sentSlotsArray[0] !== slotsInMentorSessionArray[0]) {
+        // eslint-disable-next-line no-await-in-loop
+        await callLocalGraphqlApi(updateMentorSession(mentorSessionId, date, `slot${sentSlotsArray[0]}`));
+      }
+    } else {
+      // eslint-disable-next-line no-await-in-loop
+      const addMentorSessionRes = await callLocalGraphqlApi(addMentorSession(allottedMentorId, courseId, date, `slot${sentSlotsArray[0]}`, sessionType));
+      finalMentorSessionId = get(addMentorSessionRes, 'data.addMentorSession.id');
+    }
+  }
+  return finalMentorSessionId;
+};
+
+// create batch sessions according to remaining topics and required number of sessions
+const createBatchSessions = async (batchId, possibleDates, filteredSlots, slotsInInput, possibleSessionCount, topics, allottedMentorId, courseId, batchType) => {
+  if (possibleDates.length <= possibleSessionCount) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const date of possibleDates) {
+      const index = possibleDates.indexOf(date);
+      const topicOrder = topics[index].order;
+      let sessionType = 'batch';
+
+      if (batchType === 'b2b2c' && topicOrder === 1) {
+        sessionType = 'trial';
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      const finalMentorSessionId = await getMentorSessionId(allottedMentorId, date, slotsInInput, courseId, sessionType);
+      createBatchSession(batchId, date, filteredSlots, topics[index].id, finalMentorSessionId, courseId);
+    }
+  } else {
+    for (let i = 0; i < possibleSessionCount; i += 1) {
+      const topicOrder = topics[i].order;
+      let sessionType = 'batch';
+
+      if (batchType === 'b2b2c' && topicOrder === 1) {
+        sessionType = 'trial';
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      const finalMentorSessionId = await getMentorSessionId(allottedMentorId, possibleDates[i], slotsInInput, courseId, sessionType);
+      createBatchSession(batchId, possibleDates[i].toISOString(), filteredSlots, topics[i].id, finalMentorSessionId, courseId);
+    }
+  }
   return true;
+};
+
+// update existing batch sessions with topic id
+const updateAllottedBatchSessions = async (sessionsAllotted, possibleDates, filteredSlotsString, slotsInInput, allottedMentorId, courseId, batchType) => {
+  let i = 0;
+  /* eslint-disable array-callback-return */
+  // eslint-disable-next-line no-restricted-syntax
+  for (const session of sessionsAllotted) {
+    const topicOrder = get(session, 'topic.order', -1);
+    let sessionType = 'batch';
+
+    if (batchType === 'b2b2c' && topicOrder === 1) {
+      sessionType = 'trial';
+    }
+
+    // eslint-disable-next-line no-await-in-loop
+    const finalMentorSessionId = await getMentorSessionId(allottedMentorId, possibleDates[i], slotsInInput, courseId, sessionType);
+    /* eslint-disable array-callback-return */
+    const date = possibleDates[i].toISOString();
+    updateBatchSession(session.id, filteredSlotsString, date, finalMentorSessionId, courseId);
+    i += 1;
+  }
 };
 
 const scheduleSessionsMutationResolver = async (
@@ -137,6 +207,9 @@ const scheduleSessionsMutationResolver = async (
 ) => {
   validateAuthentication(context);
   const { input: timeTableRule } = params;
+  const courseId = get(timeTableRule, 'courseId');
+  const batchId = get(timeTableRule, 'batchId');
+  const isRecurring = get(timeTableRule, 'isRecurring');
   // start, end dates
   const days = getSelectedDays(timeTableRule);
   // console.log('days', days);
@@ -148,7 +221,7 @@ const scheduleSessionsMutationResolver = async (
 
   // slots passed in input
   const { ...slots } = timeTableRule;
-  const { filteredSlots } = extractSlotsFromInput(slots);
+  const { filteredSlotsString } = extractSlotsFromInput(slots);
   // console.log('filteredSlotsString', filteredSlotsString);
 
   // Fetch Batch TimetableSchedule and School time table schedule and combine them
@@ -161,17 +234,62 @@ const scheduleSessionsMutationResolver = async (
   // See if force flag is set to false/or not sent in input
   const forceScheduleSessions = get(timeTableRule, 'forceScheduleSessions', false);
   if (!forceScheduleSessions && combinedWorkingDaySchedule.startDate && combinedWorkingDaySchedule.endDate) {
-    const isOutsideWorkingSchedule = checkIfOutsideWorkingSchedule(combinedWorkingDaySchedule, combinedEventScheduleArray, days, startDate, endDate, filteredSlots);
+    const isOutsideWorkingSchedule = checkIfOutsideWorkingSchedule(combinedWorkingDaySchedule, combinedEventScheduleArray, timeTableRule);
     if (isOutsideWorkingSchedule) {
       throw new CannotScheduleOutsideWorkingHoursError();
     }
   }
   // if force schedule, we can schedule anywhere irrespective of working day or event schedule
+  const sessionType = get(timeTableRule, 'type', 'batch');
+  if (sessionType === 'batch') {
+    if (timeTableRule) {
+      // topic count
+      let topics = await getTopics(courseId);
+      const topicCount = topics && topics.length;
+      // batch sessions
+      const batchSessions = await getBatchSessions(batchId);
 
-  // create sessions, batch / adhoc for all topics possible
-
-  // take care of shifting, in case of clash
-
+      if (batchSessions && batchSessions.length) {
+        // sorting the existing batch sessions into started/completed and allotted
+        const {
+          sessionsStartedOrCompleted,
+          sessionsAllotted,
+        } = sortBatchSessions(batchSessions);
+        let possibleSessionCount = topicCount;
+        if (sessionsStartedOrCompleted.length > 0) {
+          // if there exists some started or completed sessions, don't count them, create/update sessions for the remaining
+          possibleSessionCount -= sessionsStartedOrCompleted.length;
+        }
+        let possibleDates = getPossibleDates(startDate, endDate, days);
+        // for the sessions which are still in the allotted state, update them
+        const allottedSessionsCount = sessionsAllotted.length;
+        if (allottedSessionsCount > 0) {
+          possibleSessionCount -= allottedSessionsCount;
+          updateAllottedBatchSessions(sessionsAllotted, possibleDates, filteredSlotsString, slots, mentorUserId, courseId, batchType);
+        }
+        if (possibleSessionCount > 0) {
+          // all the remaining sessions have to be created
+          const startFromIndex = allottedSessionsCount;
+          possibleDates = possibleDates.slice(startFromIndex);
+          const topicStartIndex = topicCount - possibleSessionCount;
+          topics = topics.splice(topicStartIndex);
+          createBatchSessions(batchId, possibleDates, filteredSlotsString, slots, possibleSessionCount, topics, mentorUserId, courseId, batchType);
+        }
+      } else {
+        // if there are no exisiting batchSessions for the given batch id, create all of them
+        const possibleSessionCount = topicCount;
+        const possibleDates = getPossibleDates(startDate, endDate, days);
+        createBatchSessions(batchId, possibleDates, filteredSlotsString, slots, possibleSessionCount, topics, mentorUserId, courseId, batchType);
+      }
+    }
+  } else {
+    // do something else -> ADHOC
+    // if adhoc and recurring, return
+    if (isRecurring) {
+      return;
+    }
+    log('Create ADHOC Session');
+  }
   return {
     result: true,
   };
