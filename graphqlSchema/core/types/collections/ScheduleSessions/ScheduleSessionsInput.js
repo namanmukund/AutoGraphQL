5d33a0292f644ccc6ddabc39b5d7dfc6641b75f2
Gokul Madhusudhan <gokul99.gm@gmail.com>
2022-02-15 19:55:56 +0530

    addnl fields

--- graphqlSchema/core/types/collections/ScheduleSessions/ScheduleSessionsInput.js
+++ graphqlSchema/core/types/collections/ScheduleSessions/ScheduleSessionsInput.js
@@ -1,16 +1,22 @@
 import getSlotTimeFields from '../../../functions/getSlotTimeFields';
 import getWeekDaysFields from '../../../functions/getWeekDaysFields';
+import getWeekDaysClassModeFields from '../../../functions/getWeekDaysClassModeFields';
 
 const slotTimeFields = getSlotTimeFields('Boolean', false);
 const weekDaysFields = getWeekDaysFields('Boolean', false);
+const weekDaysClassModeFields = getWeekDaysClassModeFields('Boolean', false);
 
 const ScheduleSessionsInput = `
   input ScheduleSessionsInput {
     scheduleSessionType: ScheduleSessionType
+    adhocSessionType: AdhocSessionType
     ${slotTimeFields}
     ${weekDaysFields}
+    ${weekDaysClassModeFields}
     startDate: Date
     endDate: Date
+    startTime: Int @length(min: 0, max: 59)
+    endTime: Int @length(min: 0, max: 59)
     batchId: String
     courseId: String
     topicId: String
