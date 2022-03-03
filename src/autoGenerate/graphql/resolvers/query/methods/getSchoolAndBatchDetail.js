301de1b87e27db0e943a382fc97a66baf60a498b
Pawankumar Vishwakarma <pawan.kv612@gmail.com>
2022-03-03 11:54:46 +0530

    added: school Classes (#1423)

--- src/autoGenerate/graphql/resolvers/query/methods/getSchoolAndBatchDetail.js
+++ src/autoGenerate/graphql/resolvers/query/methods/getSchoolAndBatchDetail.js
@@ -1,16 +1,48 @@
 import { get } from 'lodash';
-import callLocalGraphqlApi from '../../../../../api/callLocalGraphqlApi';
 import validateAuthentication from '../../../../../../utils/validateAuthentication';
 import { DatabaseRecordNotFoundError } from '../../../../../../constants/errors';
 import { MissingMandatoryInputInRequestError } from '../../../../../../constants/errors/input';
+import { QueryController } from '../../../controllers';
 
-const getSchoolAndBatch = (schoolId, batchId) => `
-{
-  batches(filter: { and: [{ id: "${batchId}" }, { school_some: { id: "${schoolId}" } }] }) {
-    id
-  }
-}
-`;
+const getBatchAggregation = ({
+  batchId, schoolId,
+}) => [
+  {
+    $match: {
+      id: batchId,
+      'school.typeId': schoolId,
+    },
+  },
+  {
+    $lookup: {
+      from: 'SchoolClass',
+      localField: 'classes.typeId',
+      foreignField: 'id',
+      as: 'classes',
+    },
+  },
+  {
+    $project: {
+      id: 1,
+      classes: {
+        id: 1,
+        grade: 1,
+        section: 1,
+      },
+    },
+  },
+];
+
+const getTypeQueryController = (
+  typeName,
+  authentication = {
+    bypass: true,
+  },
+) => new QueryController(typeName, authentication);
+
+const batchModel = getTypeQueryController(
+  'Batch',
+);
 
 // this API will return basic school details
 const getSchoolAndBatchDetail = (async (root, params, context) => {
@@ -24,13 +56,21 @@ const getSchoolAndBatchDetail = (async (root, params, context) => {
     throw new MissingMandatoryInputInRequestError();
   }
 
-  const getBatchAndSchoolRes = await callLocalGraphqlApi(getSchoolAndBatch(schoolId, batchId));
-  if (!get(getBatchAndSchoolRes, 'data.batches', []).length) {
+  const batchData = await batchModel.aggregate(
+    getBatchAggregation({
+      batchId, schoolId,
+    }),
+  );
+  if (!batchData.length) {
     throw new DatabaseRecordNotFoundError();
   }
-  return {
-    result: true,
+  const batchClasses = get(batchData, '[0].classes', []);
+  const result = {
+    batchId,
+    schoolId,
+    batchClasses: batchClasses.map((batchClass) => ({ grade: get(batchClass, 'grade'), section: get(batchClass, 'section') })),
   };
+  return result;
 });
 
 export default getSchoolAndBatchDetail;
