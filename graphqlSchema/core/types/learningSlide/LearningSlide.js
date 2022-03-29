1bc936d3651ab352625f6e134476681ba34e0071
Pawankumar Vishwakarma <pawan.kv612@gmail.com>
2022-03-29 18:26:52 +0530

    schema updated to twoWay (#1459)

--- graphqlSchema/core/types/learningSlide/LearningSlide.js
+++ graphqlSchema/core/types/learningSlide/LearningSlide.js
@@ -7,7 +7,7 @@ const LearningSlide = `
     order: Int
     status: ContentStatus! @defaultValue(value: "unpublished")
     slideContents: [LearningSlideContent] @relation(name: "LearningSlideSlides")
-    practiceQuestions: [QuestionBank] @relation(name: "LearningSlideQuestionBank", direction: "OneWay")
+    practiceQuestions: [QuestionBank] @relation(name: "LearningSlideQuestionBank")
     topics: [Topic] @relation(name: "LearningSlideTopic")
     courses: [Course] @relation(name: "LearningSlideCourse", direction: "OneWay")
     learningObjectives: [LearningObjective] @relation(name: "LearningSlideLearningObjective")
