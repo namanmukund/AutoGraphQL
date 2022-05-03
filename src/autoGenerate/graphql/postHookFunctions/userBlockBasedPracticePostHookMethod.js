f2c98008e8c417a3aa5696bc9bea5324bbc41011
pawan-kumar-dev <pawan.kv612@gmail.com>
2022-05-03 21:23:17 +0530

    added: mentee level check for userLevel component postHook

--- src/autoGenerate/graphql/postHookFunctions/userBlockBasedPracticePostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/userBlockBasedPracticePostHookMethod.js
@@ -5,6 +5,7 @@ import {
 import getInfoFromParams from './utils/getInfoFromParams';
 import parseTopicComponentResultData from './utils/parseTopicComponentResultData';
 import callLocalGraphqlApi from '../../../api/callLocalGraphqlApi';
+import { MENTEE } from '../../../../constants/roles';
 
 // query to add UserBlockBasedPractice if it is not already present for user, blockBasedProjectId and topic id
 const addUserBlockBasedPracticeMutation = (
@@ -52,6 +53,9 @@ const userBlockBasedPracticePostHookMethod = async (input, params) => {
   if it is not already present, we will add a new document with default data
   */
   if (input && input.length) {
+    return input;
+  }
+  if (get(context, 'userRoleFromContext') && get(context, 'userRoleFromContext') !== MENTEE) {
     return input;
   }
   const resultArray = [];
--- src/autoGenerate/graphql/postHookFunctions/userBlockBasedProjectPostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/userBlockBasedProjectPostHookMethod.js
@@ -5,6 +5,7 @@ import {
 import getInfoFromParams from './utils/getInfoFromParams';
 import parseTopicComponentResultData from './utils/parseTopicComponentResultData';
 import callLocalGraphqlApi from '../../../api/callLocalGraphqlApi';
+import { MENTEE } from '../../../../constants/roles';
 
 // query to add UserBlockBasedProject if it is not already present for user, blockBasedProjectId and topic id
 const addUserBlockBasedProjectMutation = (
@@ -52,6 +53,9 @@ const userBlockBasedProjectPostHookMethod = async (input, params) => {
   if it is not already present, we will add a new document with default data
   */
   if (input && input.length) {
+    return input;
+  }
+  if (get(context, 'userRoleFromContext') && get(context, 'userRoleFromContext') !== MENTEE) {
     return input;
   }
   const resultArray = [];
