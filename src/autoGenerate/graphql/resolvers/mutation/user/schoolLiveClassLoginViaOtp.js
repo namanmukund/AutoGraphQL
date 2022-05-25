46efce45518e9dc8a079a1c51c011fa558b1baa5
pawan-kumar-dev <pawan.kv612@gmail.com>
2022-05-25 16:59:13 +0530

    extended login api for buddyLogin

--- src/autoGenerate/graphql/resolvers/mutation/user/schoolLiveClassLoginViaOtp.js
+++ src/autoGenerate/graphql/resolvers/mutation/user/schoolLiveClassLoginViaOtp.js
@@ -4,11 +4,11 @@ import getChildrenToken from './utils/getChildrenToken';
 import { createUserTokenTypeData } from '../utils/createUserTokenTypeData';
 import { QueryController } from '../../../controllers';
 import { getUserFromDBQuery } from './utils';
-import { InvalidUserIdError } from '../../../../../../constants/errors/auth';
+import { InvalidUserIdError, UserTokenNotRequiredError } from '../../../../../../constants/errors/auth';
 
 const USER_TYPE = 'User';
 
-const signupOrLoginViaOtp = async (
+const schoolLiveClassLoginViaOtp = async (
   root,
   params,
   context,
@@ -19,19 +19,39 @@ const signupOrLoginViaOtp = async (
   authentication,
 ) => {
   const { input } = params
-  if (!get(input, 'userId')) {
+  const currentUser = authentication && authentication.user;
+  if (currentUser) {
+    throw new UserTokenNotRequiredError();
+  }
+  if (!get(input, 'userId') && !get(input, 'buddyLoginInput', []).length) {
     throw new InvalidUserIdError()
   }
   Object.assign(authentication, {
     bypass: true,
   });
   const userModalQuery = new QueryController(USER_TYPE, authentication);
-  const userData = await getUserFromDBQuery({ id: get(input, 'userId') }, userModalQuery);
-  const userTokenData = createUserTokenTypeData(userData, authentication);
-  // if user is a parent then get children tokens as well
-  if (get(userData, 'role') === 'parent') {
-    userTokenData.children = await getChildrenToken(context, studentId);
+  if (get(input, 'userId')) {
+    const userData = await getUserFromDBQuery({ id: get(input, 'userId') }, userModalQuery);
+    const userTokenData = createUserTokenTypeData(userData, authentication);
+    // if user is a parent then get children tokens as well
+    if (get(userData, 'role') === 'parent') {
+      userTokenData.children = await getChildrenToken(context, studentId);
+    }
+    return userTokenData;
   }
-  return userTokenData;
+  if (get(input, 'buddyLoginInput', []).length) {
+    let userTokenData = { buddyDetails: [] }
+    for (let user of get(input, 'buddyLoginInput', [])) {
+      const userData = await getUserFromDBQuery({ id: get(user, 'userId') }, userModalQuery);
+      const userDetail = createUserTokenTypeData(userData, authentication);
+      if (get(user, 'isPrimaryUser')) {
+        userTokenData.buddyDetails.push({ ...userDetail, isPrimaryUser: true })
+        userTokenData = { ...userTokenData, ...userDetail }
+      } else {
+        userTokenData.buddyDetails.push({ ...userDetail, isPrimaryUser: false })
+      }
+    }
+    return userTokenData;
+  }
 };
-export default signupOrLoginViaOtp;
+export default schoolLiveClassLoginViaOtp;
