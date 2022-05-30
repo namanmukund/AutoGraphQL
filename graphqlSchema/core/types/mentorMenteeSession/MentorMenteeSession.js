80f0ea10f18cecd7572ce7699aa343b1451d7a28
smithg09 <smith.gajjar@gmail.com>
2022-05-30 15:34:40 +0530

    changes

--- graphqlSchema/core/types/mentorMenteeSession/MentorMenteeSession.js
+++ graphqlSchema/core/types/mentorMenteeSession/MentorMenteeSession.js
@@ -51,7 +51,7 @@ const internetSpeed = `
 }`;
 
 const MentorMenteeSession = `
-  type MentorMenteeSession @model @databaseController(mode: "aggregation") {
+  type MentorMenteeSession @model {
     course: Course @relation(name: "MentorMenteeSessionCourse", direction: "OneWay")
     coursePackage: CoursePackage @relation(name: "MentorMenteeSessionCoursePackage", direction: "OneWay")
     topic: Topic! @relation(name: "MentorMenteeSessionTopic", direction: "OneWay")
