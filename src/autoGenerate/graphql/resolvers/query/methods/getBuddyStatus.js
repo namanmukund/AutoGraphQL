e47935b68c3e012f2dd1557605487938c34b01d4
pawan-kumar-dev <pawan.kv612@gmail.com>
2022-06-09 14:47:49 +0530

    extended api for confirmPassword

--- src/autoGenerate/graphql/resolvers/query/methods/getBuddyStatus.js
+++ src/autoGenerate/graphql/resolvers/query/methods/getBuddyStatus.js
@@ -1,8 +1,12 @@
 import get from 'lodash/get';
+import bcrypt from 'bcryptjs';
+import { PasswordMismatchError, UnknownUserError, UserPasswordNotSetError } from '../../../../../../constants/errors';
 import { MissingMandatoryInputInRequestError } from '../../../../../../constants/errors/input';
 import { QueryController, MutationController } from '../../../controllers';
 
 const BATCHSESSION_TYPE = 'BatchSession';
+
+const STUDENTPROFILE_TYPE = 'StudentProfile';
 
 const checkIfExistInArray = (userId, arrayData = []) => {
   if (!userId || !arrayData.length) return false;
@@ -34,14 +38,65 @@ const getStudentLoggedInStatus = ({
   },
 ];
 
+const getStudentProfileAggregation = ({
+  userId,
+}) => [
+  {
+    $match: {
+      'user.typeId': userId,
+    },
+  },
+  {
+    $lookup: {
+      from: 'ParentProfile',
+      let: {
+        parentProfileId: '$parents.typeId',
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $in: ['$id', '$$parentProfileId'],
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: 'User',
+            localField: 'user.typeId',
+            foreignField: 'id',
+            as: 'user',
+          },
+        },
+        {
+          $project: {
+            user: {
+              id: 1,
+              password: 1,
+            },
+          },
+        },
+      ],
+      as: 'parents',
+    },
+  },
+  {
+    $project: {
+      parents: {
+        $arrayElemAt: ['$parents', 0],
+      },
+    },
+  },
+];
+
 const getBuddyStatus = async (
   root,
   params,
 ) => {
   const {
-    sessionId, userId, systemId, action,
+    sessionId, userId, systemId, action, password,
   } = params;
-  if (!sessionId || !action || !['add', 'delete', 'check'].includes(action)) {
+  if (!sessionId || !action || !['add', 'delete', 'check', 'confirmPassword'].includes(action)) {
     throw new MissingMandatoryInputInRequestError({
       data: {
         message: 'Either SessionId or action type or all missing in input',
@@ -66,6 +121,13 @@ const getBuddyStatus = async (
     throw new MissingMandatoryInputInRequestError({
       data: {
         message: 'userConnectId is missing in input',
+      },
+    });
+  }
+  if (action === 'confirmPassword' && (!userId || !password)) {
+    throw new MissingMandatoryInputInRequestError({
+      data: {
+        message: 'userConnectId or password is missing in input',
       },
     });
   }
@@ -106,6 +168,27 @@ const getBuddyStatus = async (
       });
       result = true;
     }
+  } else if (action === 'confirmPassword') {
+    // It checks the password and returns true if password matches else returns false
+    const userModal = getTypeQueryController(
+      STUDENTPROFILE_TYPE,
+    );
+    const userData = await userModal.aggregate(
+      getStudentProfileAggregation({
+        userId,
+      }),
+    );
+    if (!userData || !userData.length) {
+      throw new UnknownUserError();
+    }
+    if (!get(userData, '[0].parents.user[0].password')) {
+      throw new UserPasswordNotSetError();
+    }
+    const valid = bcrypt.compareSync(password, get(userData, '[0].parents.user[0].password'));
+    if (!valid) {
+      throw new PasswordMismatchError();
+    }
+    result = true;
   }
   return {
     result,
