43e7ba35f9535123ab9e19848a34996a2a296518
pawan-kumar-dev <pawan.kv612@gmail.com>
2022-08-08 18:05:50 +0530

    changed default value to answerContent

--- graphqlSchema/core/types/blockBasedProject/BlockBasedProject.js
+++ graphqlSchema/core/types/blockBasedProject/BlockBasedProject.js
@@ -35,7 +35,7 @@ const BlockBasedProject = `
     tags: [ContentTag] @relation(name: "BlockBasedProjectTag")
     externalDescriptionEnabled: Boolean @defaultValue(value: "false")
     embedViewHeight: Int @defaultValue(value: "100")
-    answerFormat: BlockBasedPracticeAnswerType @defaultValue(value: "answerLink")
+    answerFormat: BlockBasedPracticeAnswerType @defaultValue(value: "answerContent")
     answerFormatDescription: String @trim
   }
 `;
