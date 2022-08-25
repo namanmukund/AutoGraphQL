9693dc0deb52867d1d8499371bef82d78d17ea57
Tinku Parmar <tinkuparmar@tinku.local>
2022-08-25 12:45:59 +0530

    connect batch with new user

--- src/autoGenerate/graphql/postHookFunctions/addStudentProfilePostHookMethod.js
+++ src/autoGenerate/graphql/postHookFunctions/addStudentProfilePostHookMethod.js
@@ -1,6 +1,39 @@
 import { get } from 'lodash';
 import addUpdateSchoolClass from './utils/addUpdateSchoolClass';
 import addSchoolSessionOtpInBatchSession from './utils/addSchoolSessionOtpInBatchSession';
+import { callLocalGraphqlApi } from '../../../api';
+
+const userBatchQuery = async (schoolId, currentGrade, currentSection) => {
+  const query = `
+  query{
+    batches(filter: {
+    and: [
+      { school_some: { id: "${schoolId}" } }
+      { classes_some: { and: [{ grade: ${currentGrade} }, { section: ${currentSection} }] } }
+    ]
+  }){
+    id
+  }
+}
+  `;
+  const res = await callLocalGraphqlApi(query);
+  return get(res, 'data.batches');
+};
+
+const updateStudentProfile = async (studentId, batchConnectId) => {
+  const query = `
+  mutation{
+    updateStudentProfile(
+      id: "${studentId}"
+      batchConnectId:"${batchConnectId}"
+    ){
+      id
+    }
+  }
+  `;
+  const res = await callLocalGraphqlApi(query);
+  return get(res, 'data');
+};
 
 const addStudentProfilePostHookMethod = async (input, params) => {
   const currentSection = get(params, 'input.section');
@@ -19,6 +52,11 @@ const addStudentProfilePostHookMethod = async (input, params) => {
     Object.assign(input, { schoolClass: { type: 'SchoolClass', typeId: schoolClassId } });
     if (batchId) {
       addSchoolSessionOtpInBatchSession(batchId);
+    }
+    const batches = await userBatchQuery(schoolId, currentGrade, currentSection)
+    if (batches && batches.length > 0) {
+      const studentId = get(input, 'id')
+      updateStudentProfile(studentId, get(batches, '[0].id'))
     }
   }
 };
