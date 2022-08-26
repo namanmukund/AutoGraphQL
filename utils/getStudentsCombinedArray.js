95cc3f276a1845f4589d10f0b38b7656f1db4cd7
pawan-kumar-dev <pawan.kv612@gmail.com>
2022-08-26 15:44:18 +0530

    fixed get

--- utils/getStudentsCombinedArray.js
+++ utils/getStudentsCombinedArray.js
@@ -1,3 +1,5 @@
+import { get } from 'lodash';
+
 // Combine batchStudents into students and also filter out if duplicates
 const getStudentsCombinedArray = (input) => [...(input.students || []), ...(input.batchStudents || [])].filter((student, index, restStudentArray) => (restStudentArray || []).findIndex((restStudent) => (get(restStudent, 'id') === get(student, 'id'))) === index);
 
