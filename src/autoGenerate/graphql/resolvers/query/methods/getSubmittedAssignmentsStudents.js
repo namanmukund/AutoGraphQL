3297e91e22b3762152016424c319793af6c8a58e
Tinku Parmar <tinkuparmar@tinku.local>
2022-10-10 14:36:10 +0530

    remove type

--- src/autoGenerate/graphql/resolvers/query/methods/getSubmittedAssignmentsStudents.js
+++ src/autoGenerate/graphql/resolvers/query/methods/getSubmittedAssignmentsStudents.js
@@ -10,77 +10,76 @@ const getSubmittedAssignmentsStudents = async (
     params,
     context
 ) => {
-    const { userIds, courseId, topicId, type } = params;
+    const { userIds, courseId, topicId } = params;
     const authentication = ifAuthorized(context);
-    if (type === 'question') {
-        const userAssignmentModel = new QueryController('UserAssignment', authentication);
-        const pipeline = new AggregationBuilder('UserAssignment')
-            .Project(OnlyPayload('id', 'user', 'assignment', 'topic', 'course'))
-            .Match({ 'user.typeId': { $in: userIds }, 'topic.typeId': topicId, 'course.typeId': courseId })
-            .Lookup(EqualityPayload('Evaluation', 'evaluations', 'assignment.evaluation.typeId', 'id'))
-            .Lookup(EqualityPayload('AssignmentQuestion', 'questions', 'assignment.assignmentQuestion.typeId', 'id'))
-            .getPipeline();
-        const userAssignmentModelData = await userAssignmentModel.aggregate(pipeline);
-        let userAssignmentModelDataToSend = []
-        userAssignmentModelData && userAssignmentModelData.forEach(userAssignment => {
-            let obj = {
-                id: get(userAssignment, 'id'),
-                user: get(userAssignment, 'user')
-            }
-            let assignments = []
-            const userAssingmentAssignments = get(userAssignment, 'assignment')
-            const userAssingmentEvaluations = get(userAssignment, 'evaluations')
-            const userAssingmentQuesitons = get(userAssignment, 'questions')
-            userAssingmentAssignments && userAssingmentAssignments.forEach(assignment => {
-                if (get(assignment, 'userAnswerCodeSnippet', null) !== null && get(assignment, 'userAnswerCodeSnippet', null) !== 'null') {
-                    const evaluationToAdd = userAssingmentEvaluations && userAssingmentEvaluations.find(evaluation => get(evaluation, 'id') === get(assignment, 'evaluation.typeId'))
-                    const questionToAdd = userAssingmentQuesitons && userAssingmentQuesitons.find(question => get(question, 'id') === get(assignment, 'assignmentQuestion.typeId'))
-                    let assignmentObj = {
-                        isAttempted: get(assignment, 'isAttempted'),
-                        userAnswerCodeSnippet: get(assignment, 'userAnswerCodeSnippet')
-                    }
-                    assignmentObj.evaluation = evaluationToAdd
-                    assignmentObj.assignmentQuestion = questionToAdd
-                    assignments.push(assignmentObj)
+    const userAssignmentModel = new QueryController('UserAssignment', authentication);
+    const UserAssignmentPipeline = new AggregationBuilder('UserAssignment')
+        .Project(OnlyPayload('id', 'user', 'assignment', 'topic', 'course'))
+        .Match({ 'user.typeId': { $in: userIds }, 'topic.typeId': topicId, 'course.typeId': courseId })
+        .Lookup(EqualityPayload('Evaluation', 'evaluations', 'assignment.evaluation.typeId', 'id'))
+        .Lookup(EqualityPayload('AssignmentQuestion', 'questions', 'assignment.assignmentQuestion.typeId', 'id'))
+        .getPipeline();
+    const userAssignmentModelData = await userAssignmentModel.aggregate(UserAssignmentPipeline);
+    let userAssignmentModelDataToSend = []
+    userAssignmentModelData && userAssignmentModelData.forEach(userAssignment => {
+        let obj = {
+            id: get(userAssignment, 'id'),
+            user: get(userAssignment, 'user')
+        }
+        let assignments = []
+        const userAssingmentAssignments = get(userAssignment, 'assignment')
+        const userAssingmentEvaluations = get(userAssignment, 'evaluations')
+        const userAssingmentQuesitons = get(userAssignment, 'questions')
+        userAssingmentAssignments && userAssingmentAssignments.forEach(assignment => {
+            if (get(assignment, 'userAnswerCodeSnippet', null) !== null && get(assignment, 'userAnswerCodeSnippet', null) !== 'null') {
+                const evaluationToAdd = userAssingmentEvaluations && userAssingmentEvaluations.find(evaluation => get(evaluation, 'id') === get(assignment, 'evaluation.typeId'))
+                const questionToAdd = userAssingmentQuesitons && userAssingmentQuesitons.find(question => get(question, 'id') === get(assignment, 'assignmentQuestion.typeId'))
+                let assignmentObj = {
+                    isAttempted: get(assignment, 'isAttempted'),
+                    userAnswerCodeSnippet: get(assignment, 'userAnswerCodeSnippet')
                 }
-            })
-            if (assignments.length) {
-                obj.assignment = assignments
-                userAssignmentModelDataToSend.push(obj)
+                assignmentObj.evaluation = evaluationToAdd
+                assignmentObj.assignmentQuestion = questionToAdd
+                assignments.push(assignmentObj)
             }
         })
-        return {
-            userAssignment: userAssignmentModelDataToSend
+        if (assignments.length) {
+            obj.assignment = assignments
+            userAssignmentModelDataToSend.push(obj)
         }
-    } else if (type === 'practice') {
-        const userBlockBasedPracticeModel = new QueryController('UserBlockBasedPractice', authentication);
-        const pipeline = new AggregationBuilder('UserBlockBasedPractice')
-            .Project(OnlyPayload('id', 'user', 'blockBasedPractice', 'evaluation', 'topic', 'course', 'answerLink', 'savedBlocks', 'attachments'))
-            .Match({ 'user.typeId': { $in: userIds }, 'topic.typeId': topicId, 'course.typeId': courseId })
-            .Lookup(EqualityPayload('Evaluation', 'evaluation', 'evaluation.typeId', 'id'))
-            .Lookup(EqualityPayload('BlockBasedProject', 'blockBasedPractice', 'blockBasedPractice.typeId', 'id'))
-            .Lookup(EqualityPayload('File', 'attachments', 'attachments.typeId', 'id'))
-            // .Project({ ...OnlyPayload('id'), blockBasedPractice: ArrayElemAt(['$blockBasedPractice', 0], 0) })
-            .getPipeline();
-        const userBlockBasedPracticeModelData = await userBlockBasedPracticeModel.aggregate(pipeline);
-        const userBlockBasedPracticeModelDataFiltered = userBlockBasedPracticeModelData && userBlockBasedPracticeModelData.filter(userBlockBasedData => (get(userBlockBasedData, 'answerLink', null) !== null || get(userBlockBasedData, 'attachments', []).length))
-        let userBlockBasedPracticeDataToSend = []
-        userBlockBasedPracticeModelDataFiltered && userBlockBasedPracticeModelDataFiltered.forEach(practice => {
-            let obj = {
-                id: get(practice, 'id'),
-                user: get(practice, 'user'),
-                topic: get(practice, 'topic'),
-                course: get(practice, 'course'),
-                blockBasedPractice: get(practice, 'blockBasedPractice[0]', {}),
-                evaluation: get(practice, 'evaluation[0]', {}),
-                attachments: get(practice, 'attachments', {}),
-                answerLink: get(practice, 'answerLink')
-            }
-            userBlockBasedPracticeDataToSend.push(obj)
+    })
+    const userBlockBasedPracticeModel = new QueryController('UserBlockBasedPractice', authentication);
+    const UserBlockBasedPracticePipeline = new AggregationBuilder('UserBlockBasedPractice')
+        .Project(OnlyPayload('id', 'user', 'blockBasedPractice', 'evaluation', 'topic', 'course', 'answerLink', 'savedBlocks', 'attachments'))
+        .Match({ 'user.typeId': { $in: userIds }, 'topic.typeId': topicId, 'course.typeId': courseId })
+        .Lookup(EqualityPayload('Evaluation', 'evaluation', 'evaluation.typeId', 'id'))
+        .Lookup(EqualityPayload('BlockBasedProject', 'blockBasedPractice', 'blockBasedPractice.typeId', 'id'))
+        .Lookup(EqualityPayload('File', 'attachments', 'attachments.typeId', 'id'))
+        .Project({ 
+            ...OnlyPayload('id', 'user', 'topic', 'course', 'answerLink', 'savedBlocks', 'attachments'),
+                blockBasedPractice: ArrayElemAt(['$blockBasedPractice', 0], 0) ,
+                evaluation: ArrayElemAt(['$evaluation', 0], 0) 
         })
-        return {
-            blockBasedPracitce: userBlockBasedPracticeDataToSend
+        .getPipeline();
+    const userBlockBasedPracticeModelData = await userBlockBasedPracticeModel.aggregate(UserBlockBasedPracticePipeline);
+    const userBlockBasedPracticeModelDataFiltered = userBlockBasedPracticeModelData && userBlockBasedPracticeModelData.filter(userBlockBasedData => (get(userBlockBasedData, 'answerLink', null) !== null || get(userBlockBasedData, 'attachments', []).length))
+    let userBlockBasedPracticeDataToSend = []
+    userBlockBasedPracticeModelDataFiltered && userBlockBasedPracticeModelDataFiltered.forEach(practice => {
+        let obj = {
+            id: get(practice, 'id'),
+            user: get(practice, 'user'),
+            topic: get(practice, 'topic'),
+            course: get(practice, 'course'),
+            blockBasedPractice: get(practice, 'blockBasedPractice[0]', {}),
+            evaluation: get(practice, 'evaluation[0]', {}),
+            attachments: get(practice, 'attachments', {}),
+            answerLink: get(practice, 'answerLink')
         }
+        userBlockBasedPracticeDataToSend.push(obj)
+    })
+    return {
+        userAssignment: userAssignmentModelDataToSend,
+        blockBasedPracitce: userBlockBasedPracticeDataToSend
     }
 };
 
