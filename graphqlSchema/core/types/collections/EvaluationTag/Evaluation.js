875b24a54807f6c2da274615fb76b8dcaf1d2582
Tinku Parmar <tinkuparmar@tinku.local>
2022-10-18 11:23:56 +0530

    fix evaluation bugs

--- graphqlSchema/core/types/collections/EvaluationTag/Evaluation.js
+++ graphqlSchema/core/types/collections/EvaluationTag/Evaluation.js
@@ -1,6 +1,6 @@
 const Evaluation = `
     type Evaluation @model {
-        star: Int @length(min: 1, max: 5)
+        star: Int @length(min: 0, max: 5)
         tags: [EvaluationTag] @relation(name: "EvaluationResultTag", direction: "OneWay")
         comment: String
         userPractice: UserBlockBasedPractice @relation(name: "UserBlockBasedPracticeEvaluation")
