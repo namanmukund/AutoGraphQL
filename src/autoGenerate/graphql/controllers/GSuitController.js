0b64f1f4ee56c1d5fbfe118ee62b7295b2d43e69
UOLO <uolo@UOLOs-MacBook-Air.local>
2022-11-04 16:29:13 +0530

    Fix eslint error

--- src/autoGenerate/graphql/controllers/GSuitController.js
+++ src/autoGenerate/graphql/controllers/GSuitController.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { google } from 'googleapis';
 
 // const nameCase = (result) => {
@@ -124,127 +123,128 @@ class GSuitController {
     #auth;
 
     constructor() {
-        this.#auth = new google.auth.GoogleAuth({
-            keyFile: './src/autoGenerate/graphql/controllers/credentials.json',
-            scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.appdata','https://www.googleapis.com/auth/drive.photos.readonly'],
-        });
+      this.#auth = new google.auth.GoogleAuth({
+        keyFile: './src/autoGenerate/graphql/controllers/credentials.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.appdata', 'https://www.googleapis.com/auth/drive.photos.readonly'],
+      });
     }
 
+    // eslint-disable-next-line class-methods-use-this
     validateType(type) {
-        if (['drive','spreadsheet','presentation','document'].includes(type)) return true;
-        return false;
+      if (['drive', 'spreadsheet', 'presentation', 'document'].includes(type)) return true;
+      return false;
     }
 
     getClientInstanceByType = (type, version) => {
-        const auth = this.#auth;
-        if (!this.validateType(type)) throw Error(`[${type}]({ ${version}, ${auth} }) Invalid Instance Type`);
-        return google[type]({ version, auth });
+      const auth = this.#auth;
+      if (!this.validateType(type)) throw Error(`[${type}]({ ${version}, ${auth} }) Invalid Instance Type`);
+      return google[type]({ version, auth });
     }
 
     getDriveFiles = (ID_OF_THE_FOLDER) => {
-        const drives = this.getClientInstanceByType("drive","v3")
-        if(ID_OF_THE_FOLDER)
-            return drives.files.list({
-              fields : '*',
-              q: `'${ID_OF_THE_FOLDER}' in parents and trashed=false`
-            });
-        else 
-            return drives.files.list({
-              fields : '*',
-            });
+      const drives = this.getClientInstanceByType('drive', 'v3');
+      if (ID_OF_THE_FOLDER) {
+        return drives.files.list({
+          fields: '*',
+          q: `'${ID_OF_THE_FOLDER}' in parents and trashed=false`,
+        });
+      }
+      return drives.files.list({
+        fields: '*',
+      });
     }
 
     // ID_OF_THE_FOLDER is related to id of it's parent folder
-    createFileOrFolder = async (name,mimeType, parentId) => {
-        const drives = this.getClientInstanceByType("drive","v3")
-        let requestBody = {}
-        if (parentId){
-         requestBody = {
-            mimeType: `application/vnd.google-apps.${mimeType}`,
-            name,
-            parents: [parentId]
-         }
-        }else {
-            requestBody = {
-                mimeType:`application/vnd.google-apps.${mimeType}`,
-                name
-            }
-        }
-        const newFolder = await drives.files.create({
-          fields : '*',
-          requestBody: requestBody
-        });
-
-        return newFolder;
-    }
-
-    updateParentDirectory = async (childId,parentId) => {
-        const drives = this.getClientInstanceByType("drive","v3");
-        return drives.files.update({
-            fileId: childId,
-            addParents: parentId,
-            fields: 'id, parents'
-        })
-    }
-
-    duplicateFileOrFolder = async (ID_OF_THE_FILE,name,parentId) => {
-        const drives = this.getClientInstanceByType("drive","v3");
-        let requestBody = {};
-        if(parentId){
-          requestBody = {
-            name,
-            parents: [parentId]
-         }
-        }else{
-          requestBody ={
-            name
-          }
-        }
-        const newFile = await drives.files.copy({
-          fields : '*',
-          fileId:ID_OF_THE_FILE,
-          requestBody: requestBody
-        })
-        return newFile;
-    }
-
-    //properties are obj of {type,role,emailAddress,domain} if type is "user" or "group" must provide emailAddress and for "domain" provide domain
-    updatePermission = async (id,properties) => {
-        const drives = this.getClientInstanceByType("drive","v3");
-        let resource = {};
-
-        if(properties.type == "user" || properties.type == "group"){
-            resource = {
-                role: properties.role,
-                type: properties.type,
-                emailAddress: properties.emailAddress
-            };
-        }else if(properties.type == "domian"){
-            resource = {
-                role: properties.role,
-                type: properties.type,
-                domain: properties.domain
-            };
-        }else {
-            resource =  {
-                role: properties.role,
-                type: 'anyone'
-            };
-        }
-        return drives.permission.create({
-          fields : '*',
-          fileId: id,
-          resource
-        })   ; 
-    }
-    deleteFileOrFolder = async (id) =>{
-      const drives = this.getClientInstanceByType("drive","v3");
-      return await drives.files.delete({
+    createFileOrFolder = async (name, mimeType, parentId) => {
+      const drives = this.getClientInstanceByType('drive', 'v3');
+      let requestBody = {};
+      if (parentId) {
+        requestBody = {
+          mimeType: `application/vnd.google-apps.${mimeType}`,
+          name,
+          parents: [parentId],
+        };
+      } else {
+        requestBody = {
+          mimeType: `application/vnd.google-apps.${mimeType}`,
+          name,
+        };
+      }
+      const newFolder = await drives.files.create({
         fields: '*',
-        fileId: id
+        requestBody,
+      });
+
+      return newFolder;
+    }
+
+    updateParentDirectory = async (childId, parentId) => {
+      const drives = this.getClientInstanceByType('drive', 'v3');
+      return drives.files.update({
+        fileId: childId,
+        addParents: parentId,
+        fields: 'id, parents',
       });
     }
- 
-} 
+
+    duplicateFileOrFolder = async (ID_OF_THE_FILE, name, parentId) => {
+      const drives = this.getClientInstanceByType('drive', 'v3');
+      let requestBody = {};
+      if (parentId) {
+        requestBody = {
+          name,
+          parents: [parentId],
+        };
+      } else {
+        requestBody = {
+          name,
+        };
+      }
+      const newFile = await drives.files.copy({
+        fields: '*',
+        fileId: ID_OF_THE_FILE,
+        requestBody,
+      });
+      return newFile;
+    }
+
+    // properties are obj of {type,role,emailAddress,domain} if type is "user" or "group" must provide emailAddress and for "domain" provide domain
+    updatePermission = async (id, properties) => {
+      const drives = this.getClientInstanceByType('drive', 'v3');
+      let resource = {};
+
+      if (properties.type === 'user' || properties.type === 'group') {
+        resource = {
+          role: properties.role,
+          type: properties.type,
+          emailAddress: properties.emailAddress,
+        };
+      } else if (properties.type === 'domian') {
+        resource = {
+          role: properties.role,
+          type: properties.type,
+          domain: properties.domain,
+        };
+      } else {
+        resource = {
+          role: properties.role,
+          type: 'anyone',
+        };
+      }
+      return drives.permission.create({
+        fields: '*',
+        fileId: id,
+        resource,
+      });
+    }
+
+    deleteFileOrFolder = async (id) => {
+      const drives = this.getClientInstanceByType('drive', 'v3');
+      return drives.files.delete({
+        fields: '*',
+        fileId: id,
+      });
+    }
+}
 
 export default GSuitController;
