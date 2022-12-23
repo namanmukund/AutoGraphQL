a2dde821e7bce0c020cd1232560e5645dbdffe7d
smithg09 <smith.gajjar@gmail.com>
2022-12-23 13:49:16 +0530

    Debug #6

--- src/autoGenerate/graphql/resolvers/query/methods/getClassroomReport.js
+++ src/autoGenerate/graphql/resolvers/query/methods/getClassroomReport.js
@@ -726,38 +726,38 @@ const classroomReport = async (_root, params, context) => {
           pqSubmittedCount: get(previousBlockBasedObj, 'pqSubmittedCount', 0) || 0,
           pqUnattemptedCount: get(previousBlockBasedObj, 'pqUnattemptedCount', 0) || 0,
           pqQuestions: new Map(),
-          pqSubmissions: get(previousBlockBasedObj, 'pqSubmissions', new Map()) || new Map(),
+          pqSubmissions: get(previousBlockBasedObj, 'pqSubmissions') || new Map(),
         };
         let hasUserSubmittedPracticeLink = false;
-        // if (get(userbbPractice, 'blockBasedPractice.isSubmitAnswer')) {
-        //   const bbPracticeLayout = get(userbbPractice, 'blockBasedPractice.layout');
-        //   if (bbPracticeLayout === 'externalPlatform') {
-        //     if (get(userbbPractice, 'answerLink')) hasUserSubmittedPracticeLink = true;
-        //   } else if (get(userbbPractice, 'savedBlocks') || get(userbbPractice, 'attachments', []).length) {
-        //     hasUserSubmittedPracticeLink = true;
-        //   }
-        // } else {
-        //   hasUserSubmittedPracticeLink = true;
-        // }
+        if (get(userbbPractice, 'blockBasedPractice.isSubmitAnswer')) {
+          const bbPracticeLayout = get(userbbPractice, 'blockBasedPractice.layout');
+          if (bbPracticeLayout === 'externalPlatform') {
+            if (get(userbbPractice, 'answerLink')) hasUserSubmittedPracticeLink = true;
+          } else if (get(userbbPractice, 'savedBlocks') || get(userbbPractice, 'attachments', []).length) {
+            hasUserSubmittedPracticeLink = true;
+          }
+        } else {
+          hasUserSubmittedPracticeLink = true;
+        }
         innerObj.title = get(userbbPractice, 'blockBasedPractice.title', '');
         // individual questions
-        if (innerObj.pqQuestions.has(get(userbbPractice, 'blockBasedPractice.id'))) {
-          if (get(userbbPractice, 'blockBasedPractice.isSubmitAnswer')) {
-            innerObj.pqQuestions.set(get(userbbPractice, 'blockBasedPractice.id'), innerObj.pqQuestions.get(get(userbbPractice, 'blockBasedPractice.id')) + 1);
-          }
-        } else if (get(userbbPractice, 'blockBasedPractice.isSubmitAnswer')) {
-          innerObj.pqQuestions.set(get(userbbPractice, 'blockBasedPractice.id'), 1);
-        } else {
-          innerObj.pqQuestions.set(get(userbbPractice, 'blockBasedPractice.id'), 0);
-        }
-        if (get(userbbPractice, 'blockBasedPractice') && hasUserSubmittedPracticeLink) {
-          innerObj.pqSubmittedCount += 1;
-          innerObj.pqSubmissions.set(userId, {
-            userId,
-          });
-        } else {
-          innerObj.pqUnattemptedCount += 1;
-        }
+        // if (innerObj.pqQuestions.has(get(userbbPractice, 'blockBasedPractice.id'))) {
+        //   if (get(userbbPractice, 'blockBasedPractice.isSubmitAnswer')) {
+        //     innerObj.pqQuestions.set(get(userbbPractice, 'blockBasedPractice.id'), innerObj.pqQuestions.get(get(userbbPractice, 'blockBasedPractice.id')) + 1);
+        //   }
+        // } else if (get(userbbPractice, 'blockBasedPractice.isSubmitAnswer')) {
+        //   innerObj.pqQuestions.set(get(userbbPractice, 'blockBasedPractice.id'), 1);
+        // } else {
+        //   innerObj.pqQuestions.set(get(userbbPractice, 'blockBasedPractice.id'), 0);
+        // }
+        // if (get(userbbPractice, 'blockBasedPractice') && hasUserSubmittedPracticeLink) {
+        //   innerObj.pqSubmittedCount += 1;
+        //   innerObj.pqSubmissions.set(userId, {
+        //     userId,
+        //   });
+        // } else {
+        //   innerObj.pqUnattemptedCount += 1;
+        // }
         obj.blockBasedPractice.set(get(userbbPractice, 'blockBasedPractice.id'), innerObj);
       }
     }
