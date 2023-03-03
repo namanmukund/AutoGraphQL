0882840d1751d1c9965e60699f3557ecb80c57c8
pcmourya <prakashiaf1999@gmail.com>
2023-03-03 10:58:49 +0530

    Implemented single auth token for buddy.

--- src/autoGenerate/graphql/resolvers/utils/getTokenForLoginLink.js
+++ src/autoGenerate/graphql/resolvers/utils/getTokenForLoginLink.js
@@ -3,15 +3,15 @@ import jwt from 'jsonwebtoken';
 import moment from 'moment';
 import coreAuthParams from '../../../../../config/authParams';
 
-const getTokenForLoginLink = (user, createdAt, expiresIn) => {
-  const userInfo = pick(user, ['id', 'username']);
+const getTokenForLoginLink = (users, createdAt, expiresIn) => {
+  const usersInfo = (users && users.length > 1) ? users.map((user) => pick(user, ['id', 'username'])) : pick(users, ['id', 'username']);
   const linkTokenSecret = coreAuthParams.LINK_TOKEN_SECRET;
   // always taking expire value in hours
   const linkToken = jwt.sign(
     {
       linkData: {
         expiresIn: moment(createdAt).add(expiresIn, 'hours'),
-        userInfo,
+        usersInfo,
         createdAt: new Date(createdAt),
       },
     },
