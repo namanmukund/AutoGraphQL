e04060b000e8c88b870164c61c0b08a00641d8e6
satyamchaudharydev <satyamchaudharydev@gmail.com>
2023-04-26 13:51:11 +0530

    changes

--- src/connectDB.js
+++ src/connectDB.js
@@ -43,7 +43,8 @@ mongoose.on('error', (err) => {
     createScheduler('autoDeleteBlacklistedTokens');
     reRunJobsFromDB();
   }
-  if(process.env.STAGING_INSTANCE ||  process.env.STAGING_INSTANCE === undefined){
+
+  if ((process.env.STAGING_INSTANCE && process.env.STAGING_INSTANCE === 'true') || process.env.STAGING_INSTANCE === undefined) {
     createScheduler(TAT);
   }
   if (process.env.NODE_ENV === 'production' && process.env.SECONDARY_APPLICATION_NAME === TAT) {
