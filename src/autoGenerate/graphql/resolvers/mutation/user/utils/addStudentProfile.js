e39c76bf573e51ca9611f1506e188c9bf12954bd
pawan kumar <pawankumar@pawans-MacBook-Pro.local>
2023-05-07 19:17:05 +0530

    fixed addStudent

--- src/autoGenerate/graphql/resolvers/mutation/user/utils/addStudentProfile.js
+++ src/autoGenerate/graphql/resolvers/mutation/user/utils/addStudentProfile.js
@@ -33,7 +33,7 @@ mutation($input: StudentProfileInput!){
   ${schoolConnectId}
   ${batchConnectId}
   ${bookingAgentConnectId}
-  academicYearsConnectIds: ["${academicYearId}"]
+  ${schoolConnectId ? `academicYearsConnectIds: ["${academicYearId}"]` : ''}
   ){
     id
   }
