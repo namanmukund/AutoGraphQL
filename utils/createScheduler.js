991aba442e03bd0f2ac260875adbbe1d30417d31
pawan kumar <pawankumar@pawans-MacBook-Pro.local>
2023-05-22 11:51:04 +0530

    commented other schedulers

--- utils/createScheduler.js
+++ utils/createScheduler.js
@@ -8,8 +8,8 @@ import scheduleMentorReport from './scheduleJobs/scheduleMentorReport';
 // import scheduleEventSessionRemainder from './scheduleJobs/scheduleEventSessionRemainder';
 import scheduleBatchSessionOtpGenerator from './scheduleJobs/secheduleBatchSessionOtpGenerator';
 // import scheduleUpdateLeadSource from './scheduleJobs/scheduleUpdateLeadSource';
-import scheduleUpdateTheoryClassStatus from './scheduleJobs/scheduleUpdateTheoryClassStatus';
-import scheduleB2cBatchSessionReport from './scheduleJobs/scheduleB2cBatchSessionReport';
+// import scheduleUpdateTheoryClassStatus from './scheduleJobs/scheduleUpdateTheoryClassStatus';
+// import scheduleB2cBatchSessionReport from './scheduleJobs/scheduleB2cBatchSessionReport';
 import deleteUserBlacklistedTokens from './scheduleJobs/deleteUserBlacklistedTokens';
 // import batchAndUpdateUserSessionReports from './scheduleJobs/jobs/batchAndUpdateUserSessionReports';
 import { TAT } from '../constants';
@@ -120,28 +120,28 @@ const createScheduler = (schedulerName) => {
     //     await scheduleUpdateLeadSource();
     //   });
     //   break;
-    case 'autoCompleteThoeryClassroomSessions':
-      rule.tz = 'Asia/Kolkata';
-      rule.hour = 2;
-      rule.dayOfWeek = new schedule.Range(0, 6);
-      // eslint-disable-next-line no-unused-vars
-      schedule.scheduleJob(rule, async () => {
-        // eslint-disable-next-line no-console
-        console.log('scheduler started for: ', schedulerName);
-        await scheduleUpdateTheoryClassStatus();
-      });
-      break;
-    case 'b2cBatchSessionReport':
-      rule.tz = 'Asia/Kolkata';
-      rule.hour = 3;
-      rule.dayOfWeek = new schedule.Range(0, 6);
-      // eslint-disable-next-line no-unused-vars
-      schedule.scheduleJob(rule, async () => {
-        // eslint-disable-next-line no-console
-        console.log('scheduler started for: ', schedulerName);
-        await scheduleB2cBatchSessionReport();
-      });
-      break;
+    // case 'autoCompleteThoeryClassroomSessions':
+    //   rule.tz = 'Asia/Kolkata';
+    //   rule.hour = 2;
+    //   rule.dayOfWeek = new schedule.Range(0, 6);
+    //   // eslint-disable-next-line no-unused-vars
+    //   schedule.scheduleJob(rule, async () => {
+    //     // eslint-disable-next-line no-console
+    //     console.log('scheduler started for: ', schedulerName);
+    //     await scheduleUpdateTheoryClassStatus();
+    //   });
+    //   break;
+    // case 'b2cBatchSessionReport':
+    //   rule.tz = 'Asia/Kolkata';
+    //   rule.hour = 3;
+    //   rule.dayOfWeek = new schedule.Range(0, 6);
+    //   // eslint-disable-next-line no-unused-vars
+    //   schedule.scheduleJob(rule, async () => {
+    //     // eslint-disable-next-line no-console
+    //     console.log('scheduler started for: ', schedulerName);
+    //     await scheduleB2cBatchSessionReport();
+    //   });
+    //   break;
     case TAT:
       // eslint-disable-next-line no-console
       rule.tz = 'Asia/Kolkata';
